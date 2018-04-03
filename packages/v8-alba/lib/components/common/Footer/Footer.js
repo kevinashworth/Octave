2020dc8f391ddb79093edbeb86a57321d5eea4a4
Kevin Ashworth <kevinashworth@yahoo.com>
2018-04-03 15:19:26 -0700

    Rebrand the Footer

--- packages/v8-alba/lib/components/common/Footer/Footer.js
+++ packages/v8-alba/lib/components/common/Footer/Footer.js
@@ -5,8 +5,8 @@ class Footer extends Component {
   render() {
     return (
       <footer className="app-footer">
-        <span><a href="https://genesisui.com">Alba</a> &copy; 2018 creativeLabs.</span>
-        <span className="ml-auto">Powered by <a href="https://genesisui.com">GenesisUI</a></span>
+        <span><a href="https://placeholder.com/">The Fourth Wall</a> &copy; 2018 SuperSecretSite.</span>
+        <span className="ml-auto">Powered by <a href="https://placeholder.com/">4</a></span>
       </footer>
     )
   }
