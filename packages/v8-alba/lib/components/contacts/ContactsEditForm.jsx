e2c0e1b9d6b91699b0bef64965830a8a9152412a
Kevin Ashworth <kevinashworth@yahoo.com>
2018-05-03 16:06:45 -0700

    WIP - trying some tweaks to ContactEditForm

--- packages/v8-alba/lib/components/contacts/ContactsEditForm.jsx
+++ packages/v8-alba/lib/components/contacts/ContactsEditForm.jsx
@@ -1,5 +1,6 @@
 import { Components, getFragment, registerComponent  } from "meteor/vulcan:core";
 import React from 'react';
+import { withRouter } from 'react-router';
 import Contacts from '../../modules/contacts/collection.js';
 
 const ContactsEditForm = ({documentId, toggle, params}) =>
@@ -9,8 +10,27 @@ const ContactsEditForm = ({documentId, toggle, params}) =>
     mutationFragment={getFragment('ContactsEditFragment')}
     showRemove={true}
     successCallback={document => {
-      toggle();
+      // toggle();
+      this.props.router.push('/');
     }}
+
+    // successCallback={post => {
+    //   this.props.closeModal();
+    //   this.props.flash(this.context.intl.formatMessage({ id: 'posts.edit_success' }, { title: post.title }), 'success');
+    // }}
+    // removeSuccessCallback={({ documentId, documentTitle }) => {
+    //   // post edit form is being included from a single post, redirect to index
+    //   // note: this.props.params is in the worst case an empty obj (from react-router)
+    //   if (this.props.params._id) {
+    //     this.props.router.push('/');
+    //   }
+    //
+    //   const deleteDocumentSuccess = this.context.intl.formatMessage({ id: 'posts.delete_success' }, { title: documentTitle });
+    //   this.props.flash(deleteDocumentSuccess, 'success');
+    //   // todo: handle events in collection callbacks
+    //   // this.context.events.track("post deleted", {_id: documentId});
+    // }}
+
   />
 
-registerComponent('ContactsEditForm', ContactsEditForm);
+registerComponent('ContactsEditForm', ContactsEditForm, withRouter);
