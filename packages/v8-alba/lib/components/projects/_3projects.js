0ddec3fa18e565935c4966311a2daa290b1c22ff
Kevin Ashworth <kevinashworth@yahoo.com>
2018-05-04 19:03:13 -0700

    Small amount of projects for quicker testing

---
+++ packages/v8-alba/lib/components/projects/_3projects.js
@@ -0,0 +1,106 @@
+export default [
+{
+  "mtime": 1495405714.0,
+  "last_modified": "2017-05-21 15:28:34",
+  "project_id": 13662,
+  "website": "https://www.amazon.com/Patriot/dp/B017APUY62",
+  "projectTitle": "Patriot, The",
+  "projectType": "One Hour",
+  "union": "SAG-AFTRA",
+  "network": "Amazon",
+  "status": "On Hiatus",
+  "logline": "A political thriller about the Director of the State Department and his two sons, a senator and an intelligence officer working undercover as a spy...",
+  "notes": "Shoots and casts in Chicago.",
+  "season": "1",
+  "order": "10",
+  "castingCompany": "Rachel Tenner Casting",
+  "address": {
+    "street1": "5657 Wilshire Blvd., Ste. 220",
+    "street2": "",
+    "city": "Los Angeles",
+    "state": "CA",
+    "zip": "90036"
+  },
+  "personnel": [
+    {
+      "personnelId": 543,
+      "name": "Rachel Tenner",
+      "personnelTitle": "Casting Director"
+    },
+    {
+      "personnelId": 1550,
+      "name": "Charlene Lee",
+      "personnelTitle": "Casting Associate"
+    },
+    {
+      "personnelId": 2756,
+      "name": "Danny Gordon",
+      "personnelTitle": "Casting Assistant"
+    }
+  ]
+},{
+  "mtime": 1495405714.0,
+  "last_modified": "2017-05-21 15:28:34",
+  "project_id": 12970,
+  "website": "https://www.netflix.com/title/80099656",
+  "projectTitle": "Frontier",
+  "projectType": "One Hour",
+  "union": "SAG-AFTRA",
+  "network": "Netflix",
+  "status": "On Hiatus",
+  "logline": "In the late 1700's in the New World, men come scraping out a new life for themselves-lawfully or not...",
+  "notes": "Shooting in Newfoundland. (Bridget is not currently at this office.)",
+  "castingCompany": "Denise Chamian Casting",
+  "personnel": [
+    {
+      "personnelId": 724,
+      "name": "Denise Chamian",
+      "personnelTitle": "Casting Director"
+    },
+    {
+      "personnelId": 1945,
+      "name": "Beth Day",
+      "personnelTitle": "Casting Associate"
+    },
+    {
+      "personnelId": 1100,
+      "name": "Liz Ludwitzke",
+      "personnelTitle": "Casting Associate"
+    },
+    {
+      "personnelId": 2532,
+      "name": "Bridget Egan",
+      "personnelTitle": "Casting Assistant"
+    }
+  ]
+},{
+  "mtime": 1498099241.0,
+  "last_modified": "2017-06-21 19:40:41",
+  "project_id": 19003,
+  "projectTitle": "Trial By Fire",
+  "projectType": "Feature Film",
+  "union": "SAG-AFTRA",
+  "status": "Casting",
+  "logline": "Based on the trial of TODD WILMINGHAM, a Texas man executed for killing his children in a house fire. The case has been surrounded by controversy due to false evidence and testimony...",
+  "notes": "Shooting in Atlanta beginning in September. With Laura Dern, Shia Lebeouf... From Marshall Herskovitz, Edward Zwick...",
+  "castingCompany": "Vickie Thomas Casting",
+  "address": {
+    "street1": "8500 Wilshire Blvd., Ste. 521",
+    "street2": "",
+    "city": "Beverly Hills",
+    "state": "CA",
+    "zip": "90211"
+  },
+  "personnel": [
+    {
+      "personnelId": 404,
+      "name": "Vickie Thomas",
+      "personnelTitle": "Casting Director"
+    },
+    {
+      "personnelId": 1727,
+      "name": "Elizabeth Brown",
+      "personnelTitle": "Casting Associate"
+    }
+  ]
+}]
