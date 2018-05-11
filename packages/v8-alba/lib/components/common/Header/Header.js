e78e324438988ca2dd0b738d7160dfdcaad9069e
Kevin Ashworth <kevinashworth@yahoo.com>
2018-05-10 19:47:18 -0700

    Add Header in. Reverse CSS tweak

---
+++ packages/v8-alba/lib/components/common/Header/Header.js
@@ -0,0 +1,54 @@
+import { registerComponent } from 'meteor/vulcan:core';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {
+  Nav,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+import HeaderDropdown from './HeaderDropdown';
+
+class Header extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  sidebarToggle(e) {
+    e.preventDefault();
+    document.body.classList.toggle('sidebar-hidden');
+  }
+
+  sidebarMinimize(e) {
+    e.preventDefault();
+    document.body.classList.toggle('sidebar-minimized');
+  }
+
+  mobileSidebarToggle(e) {
+    e.preventDefault();
+    document.body.classList.toggle('sidebar-mobile-show');
+  }
+
+  asideToggle(e) {
+    e.preventDefault();
+    document.body.classList.toggle('aside-menu-hidden');
+  }
+
+  render() {
+    return (
+      <header className="app-header navbar">
+        <NavbarToggler className="d-lg-none" onClick={this.mobileSidebarToggle}>
+          <span className="navbar-toggler-icon"></span>
+        </NavbarToggler>
+        <NavbarBrand href="/"><img src="/img/logo-symbol.png" /></NavbarBrand>
+        <NavbarToggler className="d-md-down-none" onClick={this.sidebarToggle}>
+          <span className="navbar-toggler-icon"></span>
+        </NavbarToggler>
+      </header>
+    );
+  }
+}
+
+registerComponent('Header', Header);
--- packages/v8-alba/lib/components/common/LayoutFull/LayoutFull.js
+++ packages/v8-alba/lib/components/common/LayoutFull/LayoutFull.js
@@ -4,6 +4,7 @@ import {Container} from 'reactstrap';
 
 const LayoutFull = ({ children }) => (
   <div className="app">
+    <Components.Header />
     <div className="app-body">
       <Components.Sidebar {...this.props}/>
       <main className="main">
