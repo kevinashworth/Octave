ee0601bf351d8ab09a9122495c2c786988207245
Kevin Ashworth <kevinashworth@yahoo.com>
2018-08-06 17:44:19 -0700

    WIP - doesn't work because of Vulcan devel getting this wrong for now? - moving to 'keptState' approach

--- packages/v8-alba/lib/modules/filters/custom_user_fields.js
+++ packages/v8-alba/lib/modules/filters/custom_user_fields.js
@@ -1,131 +1,35 @@
 import { extendFragment } from 'meteor/vulcan:core';
 import Users from 'meteor/vulcan:users';
-import { PROJECT_ENUM, STATUS_ENUM } from '../constants.js';
-import _ from 'lodash';
-
-const filterProjectsByType = PROJECT_ENUM;
-
-let filterProjectsByStatus = {
-  "Casting": true,
-  "On Hold": false,
-  "Shooting": false,
-  "On Hiatus": false,
-  "See Notes": true,
-  "Unknown": true,
-  "Wrapped": false,
-  "Canceled": false,
-};
-
-  // let filterProjectsByLastUpdated = {
-  //   filterProjectsByLastUpdated: 'filterProjectsByLastUpdatedTwoWeeks'
-  // }
-
-let filterProjectsArray = [];
+import { PROJECT_ENUM } from '../constants.js';
 
 const addFields = () => {
-  // _.forEach(filterProjectsByType, function(value, key) {
-  //   const fieldName = _.camelCase(`filterProjectsByType${key}`);
-  //   filterProjectsArray.push(fieldName);
-  //   Users.addField({
-  //     fieldName: fieldName,
-  //     fieldSchema: {
-  //       label: `${key}`,
-  //       type: Boolean,
-  //       optional: true,
-  //       defaultValue: value,
-  //       control: "checkbox",
-  //       viewableBy: ["members"],
-  //       insertableBy: ["members"],
-  //       editableBy: ["members"],
-  //       // group: {
-  //       //   label: "Type",
-  //       //   name: "filterProjectsByType",
-  //       //   order: 1
-  //       // },
-  //     }
-  //   })
-  // });
-  Users.addField([{
-    fieldName: "filterProjectsByType",
-    fieldSchema: {
-      label: "filterProjectsByType",
-      type: Array,
-      optional: true,
-      defaultValue: filterProjectsByType,
-      viewableBy: ["members"],
-      insertableBy: ["members"],
-      editableBy: ["members"],
-    }
-  },
-  {
-    fieldName: 'filterProjectsByType.$',
-    fieldSchema: {
-      type: String,
-      optional: true
-    }
-  }
-  ])
-
-  _.forEach(filterProjectsByStatus, function(value, key) {
-    const fieldName = _.camelCase(`filterProjectsByStatus${key}`);
-    filterProjectsArray.push(fieldName);
-    Users.addField({
-      fieldName: fieldName,
+  // eslint-disable-next-line no-console
+  console.log('adding PROJECT_ENUM:', PROJECT_ENUM);
+  Users.addField([
+    {
+      fieldName: 'projectTypeFilters',
       fieldSchema: {
-        label: `${key}`,
-        type: Boolean,
+        type: Array,
         optional: true,
-        defaultValue: value,
-        control: "checkbox",
-        viewableBy: ["members"],
-        insertableBy: ["members"],
-        editableBy: ["members"],
-        // group: {
-        //   label: "Status",
-        //   name: "filterProjectsByStatus",
-        //   order: 2
-        // },
+        defaultValue: PROJECT_ENUM,
+        viewableBy: ['members'],
+        insertableBy: ['members'],
+        editableBy: ['members'],
       }
-    })
-  });
-
-  filterProjectsArray.push("filterProjectsByLastUpdated");
-  Users.addField({
-    fieldName: "filterProjectsByLastUpdated",
-    fieldSchema: {
-      label: "filterProjectsByLastUpdated",
-      type: String,
-      optional: true,
-      defaultValue: "filterProjectsByLastUpdatedTwoWeeks",
-      control: "text",
-      viewableBy: ["members"],
-      insertableBy: ["members"],
-      editableBy: ["members"],
-      // group: {
-      //   label: "Status",
-      //   name: "filterProjectsByStatus",
-      //   order: 2
-      // },
+    },
+    {
+      fieldName: 'projectTypeFilters.$',
+      fieldSchema: {
+        type: String,
+        optional: true
+      }
     }
-  })
-
-  const graphQLFilterList = filterProjectsArray.join().replace(/,/g, '\n');
-
-  // registerFragment(`
-  //   fragment UserProjectFiltersList on User {
-  //     _id
-  //     ${graphQLFilterList}
-  //   }
-  // `);
+  ]);
 
   extendFragment(
-    'UsersCurrent',
-    `
-    ${graphQLFilterList}
-  `
-  );
+    'UsersCurrent',`
+    projectTypeFilters
+  `);
 };
 
-
-// Add filter options for projects
 addFields();
