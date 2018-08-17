dc0b40fdd74d4b74a71dbb5979ef76e35370ce18
Kevin Ashworth <kevinashworth@yahoo.com>
2018-08-17 16:33:56 -0700

    Addresses not showing ... need view/insert/edit permissions

--- packages/v8-alba/lib/modules/shared_schemas.js
+++ packages/v8-alba/lib/modules/shared_schemas.js
@@ -3,22 +3,37 @@ import SimpleSchema from 'simpl-schema';
 export const addressSchema = new SimpleSchema({
   street1: {
     type: String,
-    optional: true
+    optional: true,
+    viewableBy: ["members"],
+    insertableBy: ["admins"],
+    editableBy: ["admins"]
   },
   street2: {
     type: String,
-    optional: true
+    optional: true,
+    viewableBy: ["members"],
+    insertableBy: ["admins"],
+    editableBy: ["admins"]
   },
   city: {
     type: String,
-    optional: true
+    optional: true,
+    viewableBy: ["members"],
+    insertableBy: ["admins"],
+    editableBy: ["admins"]
   },
   state: {
     type: String,
-    optional: true
+    optional: true,
+    viewableBy: ["members"],
+    insertableBy: ["admins"],
+    editableBy: ["admins"]
   },
   zip: {
     type: String,
-    optional: true
+    optional: true,
+    viewableBy: ["members"],
+    insertableBy: ["admins"],
+    editableBy: ["admins"]
   },
 });
