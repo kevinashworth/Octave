987257ebdc93adb4d8a20d962fbeb54630b1e762
Kevin Ashworth <kevinashworth@yahoo.com>
2018-09-07 20:43:40 -0700

    Add some static data

--- packages/v8-alba/lib/components/common/LatestUpdates/LatestUpdates.js
+++ packages/v8-alba/lib/components/common/LatestUpdates/LatestUpdates.js
@@ -1,5 +1,6 @@
 import { Components, registerComponent } from 'meteor/vulcan:core';
 import React, {Component} from 'react';
+import { Link } from 'react-router';
 import {Badge, Row, Col, Card, CardHeader, CardFooter, CardBody, Label, Input} from 'reactstrap';
 
 class LatestUpdates extends Component {
@@ -8,39 +9,55 @@ class LatestUpdates extends Component {
       <div className="animated fadeIn">
         <Row>
           <Col xs="12" sm="6" md="4">
-            <Card className="border-primary">
+            <Card className="card-accent-danger">
               <CardHeader>
-                Card outline primary
+              <b><Link to={`/projects/Sgsmc4S32AybDdRbA/politician-the`}>Politician, The</Link></b> (TV One Hour • Netflix)
               </CardHeader>
               <CardBody>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-                laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-                ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+
+Casting<br/>
+Alexa L. Fogel Casting<br/>
+5225 Wilshire Blvd<br/>
+Room 419<br/>
+Los Angeles CA 90036
               </CardBody>
+              <CardFooter>
+                <small className="text-muted"><b>New!</b> Project added Sept 4</small>
+              </CardFooter>
             </Card>
           </Col>
           <Col xs="12" sm="6" md="4">
-            <Card className="border-secondary">
+            <Card className="card-accent-danger">
               <CardHeader>
-                Card outline secondary
+              <b><Link to ={`/projects/ie8bTLRHNXDTHb5Y8/dwight-in-shining-armor`}>Dwight in Shining Armor</Link></b> (TV 1/2 Hour • BYUtv)
               </CardHeader>
               <CardBody>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-                laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-                ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+Casting<br/>
+Shoots in and around Salt Lake City, Utah<br/>
+Doro / Sherwood Casting<br/>
+Casting Director <b><Link to={`/contacts/tug6LvbQzypBbaKH9/nickole-doro`}>Nickole Doro</Link></b><br/>
+No address yet. HELP!<br/>
               </CardBody>
+              <CardFooter>
+                <small className="text-muted"><b>>New!</b> Project added Aug 28</small>
+              </CardFooter>
             </Card>
           </Col>
           <Col xs="12" sm="6" md="4">
-            <Card className="border-success">
+            <Card className="card-accent-warning">
               <CardHeader>
-                Card outline success
+                <b><Link to={`http://localhost:4004/contacts/erEkjhsHDYujxgsMC/danielle-aufiero`}>Danielle Aufiero</Link></b>
               </CardHeader>
               <CardBody>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-                laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-                ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                Casting Director<br/>
+                <b><Link to={`/projects/KhCguaTjuKQLAajJA/on-my-block`}>On My Block</Link></b><br/>
+588 N Larchmont Blvd<br/>
+2nd Floor<br/>
+Los Angeles CA 90004<br/>
               </CardBody>
+              <CardFooter>
+                <small className="text-muted">Contact updated Sept 7</small>
+              </CardFooter>
             </Card>
           </Col>
           <Col xs="12" sm="6" md="4">
@@ -49,10 +66,15 @@ class LatestUpdates extends Component {
                 Card outline info
               </CardHeader>
               <CardBody>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-                laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-                ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+              TV One Hour • Netflix
+Casting
+Details about the plot and characters in The Politician are being kept under wraps, but it revolves around the political aspirations of Ben Platt’s Payton, a wealthy Santa Barbarian, and every season will revolve around a different political race he is involved in.
+With Ben Platt; from Ryan Murphy.
+Alexa L. Fogel Casting
               </CardBody>
+              <CardFooter>
+                <small className="text-muted">Project added to database 2018-09-04</small>
+              </CardFooter>
             </Card>
           </Col>
           <Col xs="12" sm="6" md="4">
