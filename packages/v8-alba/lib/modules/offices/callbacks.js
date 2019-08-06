f877a510726c60a65694d5e09d197caaa87ed9a7
Kevin Ashworth <kevinashworth@yahoo.com>
2019-08-05 17:09:52 -0700

    Redo an `offices` callback
    - Wasn't working right, this works better
    - TODO: DRY and simplify and such

--- packages/v8-alba/lib/modules/offices/callbacks.js
+++ packages/v8-alba/lib/modules/offices/callbacks.js
@@ -3,6 +3,8 @@ import _ from 'lodash'
 import Contacts from '../contacts/collection.js'
 import Projects from '../projects/collection.js'
 import PastProjects from '../past-projects/collection.js'
+import { isEmptyValue } from '../helpers.js'
+
 
 /*
 When updating a project on an office, also update that project with the office.
@@ -47,49 +49,86 @@ function OfficeEditUpdatePastProjects (data, { document }) {
 }
 
 function OfficeEditUpdateContacts (data, { document, oldDocument }) {
-  const newOffice = document
-  const oldOffice = oldDocument
+  // [a] if the two `contacts` arrays are equal, do nothing
+  // [b] else for deleted contacts in oldOffice but not newOffice, remove office from those contacts
+  // [c] and for added contacts in newOffice but not oldOffice, add office to those contacts
 
-  if (_.isEqual(oldOffice.contacts, newOffice.contacts)) {
-    return
-  }
+  let office = document
+  let contactsToRemoveThisOfficeFrom = null
+  let contactsToAddThisOfficeTo = null
 
-  const oldOfficeContactsLength = oldOffice.contacts ? oldOffice.contacts.length : 0
-  const newOfficeContactsLength = newOffice.contacts ? newOffice.contacts.length : 0
-
-  if (oldOfficeContactsLength === newOfficeContactsLength && newOfficeContactsLength === 0) {
-    return
-  }
-
-  let office = null
-  if (oldOfficeContactsLength > newOfficeContactsLength) {
-    office = oldOffice
-  } else {
-    office = newOffice
-  }
-
-  office.contacts.forEach(officeContact => {
-    const contact = Contacts.findOne(officeContact.contactId)
-    const newOffice = {
-      officeId: office._id
-    }
-    var newOffices = []
-    // case 1:
-    if (!contact.offices) {
-      newOffices = [newOffice]
+  if (!oldDocument) { // newly created office --> [c] add office to its contacts, if any
+    if (!isEmptyValue(office.contacts)) {
+      contactsToAddThisOfficeTo = office.contacts
     } else {
-      const i = _.findIndex(contact.offices, { officeId: office._id })
-      newOffices = contact.offices
-      if (i < 0) {
-        // case 2:
-        newOffices.push(newOffice)
-      } else {
-        // case 3:
-        newOffices[i] = newOffice
-      }
+      return
     }
-    Connectors.update(Contacts, contact._id, { $set: { offices: newOffices } })
-  })
+  } else {
+    const newOffice = document
+    const oldOffice = oldDocument
+    const oldOfficeContactsLength = oldOffice.contacts ? oldOffice.contacts.length : 0
+    const newOfficeContactsLength = newOffice.contacts ? newOffice.contacts.length : 0
+    // [a]
+    if (oldOfficeContactsLength === newOfficeContactsLength && newOfficeContactsLength === 0) {
+      return
+    }
+    if (oldOfficeContactsLength && newOfficeContactsLength && _.isEqual(oldOffice.contacts, newOffice.contacts)) {
+      return
+    }
+    // [b]
+    contactsToRemoveThisOfficeFrom = _.difference(oldOffice.contacts, newOffice.contacts)
+    // [c]
+    contactsToAddThisOfficeTo = _.difference(newOffice.contacts, oldOffice.contacts)
+    console.info('OfficeEditUpdateContacts:', oldOfficeContactsLength, newOfficeContactsLength);
+    console.info('contactsToRemoveThisOfficeFrom:', contactsToRemoveThisOfficeFrom)
+    console.info('contactsToAddThisOfficeTo:', contactsToAddThisOfficeTo)
+  }
+  // [b]
+  if (contactsToRemoveThisOfficeFrom) {
+    contactsToRemoveThisOfficeFrom.forEach(deletedContact => {
+      const contact = Contacts.findOne(deletedContact.contactId)
+      // case 1: there are no offices on the contact and contact.offices is undefined (shouldn't happen, theoretically, but it does currently anyway)
+      if (!contact.offices) {
+        return
+      } else {
+        const outdatedOffices = contact.offices
+        const updatedOffices = _.remove(outdatedOffices, function(o) {
+          return o._id === office._id
+        })
+        //case 2: the office didn't have the contact on it (shouldn't happen, theoretically, but it does currently anyway)
+        if (_.isEqual(updatedOffices, outdatedOffices)) {
+          return
+        }
+        // case 3: update the contact with its new offices
+        Connectors.update(Contacts, contact._id, { $set: { offices: updatedOffices } })
+      }
+    })
+  }
+  // [c]
+  if (contactsToAddThisOfficeTo) {
+    contactsToAddThisOfficeTo.forEach(addedContact => {
+      const contact = Contacts.findOne(addedContact.contactId)
+      const updatedOffice = {
+        officeId: office._id
+      }
+      let updatedOffices = []
+      // case 1: office has no contacts, add contact
+      if (isEmptyValue(contact.offices)) {
+        updatedOffices = [updatedOffice]
+      } else {
+        updatedOffices = contact.offices
+        const i = _.findIndex(contact.offices, { officeId: office._id })
+        if (i < 0) {
+          // case 2: contact not on office list of contacts, add contact
+          updatedOffices.push(updatedOffice)
+        } else {
+          // case 3: contact is already on this office (shouldn't happen, theoretically, but it does currently anyway)
+          return
+        }
+      }
+      Connectors.update(Contacts, contact._id, { $set: { offices: updatedOffices } })
+    })
+  }
 }
 
 addCallback('office.create.after', OfficeEditUpdateContacts)
