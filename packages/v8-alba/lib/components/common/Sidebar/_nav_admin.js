c9fae4d3b49fcce626415f3c743ce80a2d63f069
Kevin Ashworth <kevinashworth@yahoo.com>
2019-08-06 17:36:29 -0700

    Add nav_admin
    - When logged in as admin

---
+++ packages/v8-alba/lib/components/common/Sidebar/_nav_admin.js
@@ -0,0 +1,19 @@
+export default {
+  items: [
+    {
+      title: true,
+      name: 'Admin'
+    },
+    {
+      name: 'User Admin',
+      url: '/admin',
+      icon: 'fa fa-user-o'
+    },
+    {
+      name: 'Statistics',
+      url: '/statistics/list',
+      icon: 'fa fa-bar-chart'
+    },
+
+  ]
+}
