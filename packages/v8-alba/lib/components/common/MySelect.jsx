d458456c0b0ac2844586f177935f283236993352
Kevin Ashworth <kevinashworth@yahoo.com>
2019-10-03 14:38:48 -0700

    New syntax
    - NOTE: required isn't available, so don't use `isClearable` just have to click red MINUS on SmartForm to ensure no selection

--- packages/v8-alba/lib/components/common/MySelect.jsx
+++ packages/v8-alba/lib/components/common/MySelect.jsx
@@ -1,21 +1,25 @@
 import { registerComponent } from 'meteor/vulcan:lib'
-// import { intlShape } from 'meteor/vulcan:i18n'
+import { intlShape } from 'meteor/vulcan:i18n'
 import React, { PureComponent } from 'react'
 import Select from 'react-select'
 import PropTypes from 'prop-types'
+import find from 'lodash/find'
 
 class MySelect extends PureComponent {
   constructor (props) {
     super(props)
     this.handleChange = this.handleChange.bind(this)
 
+    const initialValue = find(this.props.options, { value: this.props.value }) || null
+    console.info('initialValue:', initialValue)
+
     this.state = {
-      value: this.props.value
+      selectedOption: initialValue
     }
   }
 
-  handleChange (value) {
-    this.setState({ value })
+  handleChange (selectedOption) {
+    this.setState({ selectedOption })
 
     let siblingPath = null
     if (this.props.parentFieldName === 'projects' || this.props.parentFieldName === 'pastProjects') {
@@ -24,35 +28,34 @@ class MySelect extends PureComponent {
     }
     if (siblingPath) {
       this.context.updateCurrentValues({
-        [this.props.path]: value.value,
-        [siblingPath]: value.label
+        [this.props.path]: selectedOption.value,
+        [siblingPath]: selectedOption.label
       })
     } else {
-      this.context.updateCurrentValues({ [this.props.path]: value.value })
+      this.context.updateCurrentValues({ [this.props.path]: selectedOption.value })
     }
 
     console.group('MySelect:')
-    console.log(`MySelect label: ${value.label}`)
-    console.log(`MySelect value: ${value.value}`)
+    console.log(`MySelect label: ${selectedOption.label}`)
+    console.log(`MySelect value: ${selectedOption.value}`)
     console.groupEnd()
   }
 
   render () {
-    // const noneOption = {
-    //   label: this.context.intl.formatMessage({ id: 'forms.select_option' }),
-    //   value: null,
-    //   disabled: true
-    // }
+    const noneOption = {
+      label: this.context.intl.formatMessage({ id: 'forms.select_option' }),
+      value: null,
+      isDisabled: true
+    }
+    const { selectedOption } = this.state
     return (
       <div className='form-group row'>
         <label className='control-label col-sm-3'>{this.props.label}</label>
         <div className='col-sm-9'>
           <Select
-            name='form-field-name'
-            value={this.state.value}
+            value={selectedOption}
             onChange={this.handleChange}
-            options={{this.props.options}}
-            resetValue={{ value: null, label: '' }}
+            options={[noneOption, ...this.props.options]}
           />
         </div>
       </div>
@@ -61,8 +64,8 @@ class MySelect extends PureComponent {
 }
 
 MySelect.contextTypes = {
-  updateCurrentValues: PropTypes.func
-  // intl: intlShape
+  updateCurrentValues: PropTypes.func,
+  intl: intlShape
 }
 
 registerComponent('MySelect', MySelect)
