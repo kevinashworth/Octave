adaa86d7610608ddab22694d26a895221f69d261
Kevin Ashworth <kevinashworth@yahoo.com>
2019-10-26 19:56:22 -0700

    Put it initially here

--- packages/v8-alba/lib/components/common/LayoutFull/LayoutFull.js
+++ packages/v8-alba/lib/components/common/LayoutFull/LayoutFull.js
@@ -2,6 +2,20 @@ import { replaceComponent, Components } from 'meteor/vulcan:core'
 import React from 'react'
 import { Container } from 'reactstrap'
 
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
+
+const searchClient = algoliasearch('14RUP1OK0B', '2cd70a6b9a01c10aeaee6f6f11105f46');
+
+const Hit = ({ hit }) => <p>{hit.name}</p>
+
+const Algolia = () => (
+  <InstantSearch searchClient={searchClient} indexName='dev_v8-alba-mlab'>
+    <SearchBox />
+    <Hits hitComponent={Hit} />
+  </InstantSearch>
+);
+
 const LayoutFull = ({ children }) => (
   <div className='app'>
     <Components.Header />
@@ -10,6 +24,7 @@ const LayoutFull = ({ children }) => (
       <main className='main'>
         <Components.ErrorBoundary>
           <Components.FlashMessages />
+          <Algolia />
           <Container fluid>
             {children}
           </Container>
