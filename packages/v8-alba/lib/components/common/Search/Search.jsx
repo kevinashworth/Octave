8c67793d49f0d283b843ca34e6a7b114f1845769
Kevin Ashworth <kevinashworth@yahoo.com>
2019-10-28 08:37:35 -0700

    Better Search design

--- packages/v8-alba/lib/components/common/Search/Search.jsx
+++ packages/v8-alba/lib/components/common/Search/Search.jsx
@@ -1,39 +1,85 @@
 import { registerComponent, Components } from 'meteor/vulcan:core'
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
-import { Badge, Dropdown, DropdownItem, DropdownMenu, DropdownToggle } from 'reactstrap'
+import { Col, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Input, Form, FormGroup } from 'reactstrap'
 
 import algoliasearch from 'algoliasearch/lite'
-import { connectHits, connectStateResults, InstantSearch, SearchBox } from 'react-instantsearch-dom'
+import { connectHits, connectSearchBox, connectStateResults, InstantSearch } from 'react-instantsearch-dom'
+
+// <Form>
+//
+// </Form>
+// <FormGroup>
+//   <Label htmlFor='appendedInputButtons'>Two-button append</Label>
+//   <div className='controls'>
+//     <InputGroup>
+//       <Input id='appendedInputButtons' size='16' type='text'/>
+//       <div className='input-group-append'>
+//         <Button color='secondary'>Search</Button>
+//         <Button color='secondary'>Options</Button>
+//       </div>
+//     </InputGroup>
+//   </div>
+// </FormGroup>
+//
+// <DropdownToggle nav>
+//   <i className='icon-bell'></i><Badge pill color='danger'>{stuff.length}</Badge>
+// </DropdownToggle>
+
+// <InputGroup sm={10}>
+//
+// </InputGroup>
+
+// <div className='input-group-append'>
+//   <Button color='secondary'>Search</Button>
+//   <Button color='secondary'>Options</Button>
+// </div>
+
+
+
+const SearchBox = ({ currentRefinement, refine }) => (
+  <Form noValidate action='' role='search' inline={true}>
+    <FormGroup row>
+      <Col sm={10}>
+        <Input placeholder='Search'
+          type='search'
+          value={currentRefinement}
+          onChange={event => refine(event.currentTarget.value)}
+        />
+      </Col>
+      </FormGroup>
+  </Form>
+);
+
+const CustomSearchBox = connectSearchBox(SearchBox);
 
 const searchClient = algoliasearch('14RUP1OK0B', '2cd70a6b9a01c10aeaee6f6f11105f46')
 
 // const Hit = ({ hit }) => <p><Link to={hit.url}>{hit.name}</Link></p>
 
-const MyHits = (stuff) => {
+const Hits = (stuff) => {
   const { hits } = stuff
   console.log('stuff:', stuff)
   return (
-    <Dropdown nav isOpen={true}>
-      <DropdownToggle nav>
-        <i className="icon-bell"></i><Badge pill color="danger">{stuff.length}</Badge>
-      </DropdownToggle>
+    <Fragment>
+      <DropdownToggle nav></DropdownToggle>
       <DropdownMenu>
+        <DropdownItem header>Search Results</DropdownItem>
       {hits.map(hit => (
         <DropdownItem key={hit.objectID}><Link to={hit.url}>{hit.name}</Link></DropdownItem>
       ))}
       </DropdownMenu>
-    </Dropdown>
+    </Fragment>
   )
 }
 
-const TheHits = connectHits(MyHits)
+const CustomHits = connectHits(Hits)
 
 const Content = connectStateResults(
   ({ searchResults, searchState }) =>
     searchState && searchState.query
-      ? <TheHits />
-      : <div>No query</div>
+        ? <CustomHits />
+    : null
 )
 
 // const StateResults = () => {
@@ -42,8 +88,10 @@ const Content = connectStateResults(
 
 const Algolia = () => (
   <InstantSearch searchClient={searchClient} indexName='dev_v8-alba-mlab'>
-    <SearchBox />
-    <Content />
+    <Dropdown nav isOpen={true}>
+      <CustomSearchBox />
+      <Content />
+    </Dropdown>
   </InstantSearch>
 )
 
