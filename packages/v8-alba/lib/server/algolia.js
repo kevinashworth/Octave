9c117c292473a213dac30cc35e876f02ef4d88a8
Kevin Ashworth <kevinashworth@yahoo.com>
2019-10-28 11:33:27 -0700

    Run once on mlab, and then ... what?

--- packages/v8-alba/lib/server/algolia.js
+++ packages/v8-alba/lib/server/algolia.js
@@ -2,17 +2,24 @@ import algoliasearch from 'algoliasearch'
 import Contacts from '../modules/contacts/collection.js'
 import Offices from '../modules/offices/collection.js'
 import Projects from '../modules/projects/collection.js'
-import PastProjects from '../modules/past-projects/collection.js'
+// import PastProjects from '../modules/past-projects/collection.js'
+
+let applicationid = Meteor.settings.private.algolia.ApplicationID
+let adminapikey   = Meteor.settings.private.algolia.AdminAPIKey
+let algoliaindex  = Meteor.settings.private.algolia.AlgoliaIndex
 
 const createAlgoliaIndex = () => {
-  const client = algoliasearch(Meteor.settings.private.algolia.applicationid, Meteor.settings.private.algolia.adminapikey)
-  const index = client.initIndex(Meteor.settings.private.algolia.index)
+  const client = algoliasearch(applicationid, adminapikey)
+  const index = client.initIndex(algoliaindex)
   let objects = []
   Contacts.find().forEach((o) => {
     const indexedObject = {
       objectID: o._id,
       name: o.displayName,
-      url: `/contacts/${o._id}/${o.slug}`
+      url: `/contacts/${o._id}/${o.slug}`,
+      addressString: o.addressString,
+      allLinks: o.allLinks,
+      body: o.body
     }
     objects.push(indexedObject)
   })
@@ -20,7 +27,9 @@ const createAlgoliaIndex = () => {
     const indexedObject = {
       objectID: o._id,
       name: o.displayName,
-      url: `/offices/${o._id}/${o.slug}`
+      url: `/offices/${o._id}/${o.slug}`,
+      fullAddress: o.fullAddress,
+      body: o.body
     }
     objects.push(indexedObject)
   })
@@ -28,18 +37,24 @@ const createAlgoliaIndex = () => {
     const indexedObject = {
       objectID: o._id,
       name: o.projectTitle,
-      url: `/projects/${o._id}/${o.slug}`
+      url: `/projects/${o._id}/${o.slug}`,
+      network: o.network,
+      summary: o.summary,
+      notes: o.notes
     }
     objects.push(indexedObject)
   })
-  PastProjects.find().forEach((o) => {
-    const indexedObject = {
-      objectID: o._id,
-      name: o.projectTitle,
-      url: `/pastprojects/${o._id}/${o.slug}`
-    }
-    objects.push(indexedObject)
-  })
+  // PastProjects.find().forEach((o) => {
+  //   const indexedObject = {
+  //     objectID: o._id,
+  //     name: o.projectTitle,
+  //     url: `/past-projects/${o._id}/${o.slug}`,
+  //     network: o.network,
+  //     summary: o.summary,
+  //     notes: o.notes
+  //   }
+  //   objects.push(indexedObject)
+  // })
 
   index
     .saveObjects(objects)
