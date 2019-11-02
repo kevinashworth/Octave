a621333005a6a30763b42511e67feb7037c28090
Kevin Ashworth <kevinashworth@yahoo.com>
2019-11-02 16:47:24 -0600

    Comment out ineffective / unused process.env code
    - Too late to set MONGO_URL by the time you have access to Meteor

--- packages/v8-alba/lib/server/main.js
+++ packages/v8-alba/lib/server/main.js
@@ -1,6 +1,6 @@
-let mongoPassword = Meteor.settings.private.mlabpass
-let mongoUsername = Meteor.settings.private.mlabuser
-process.env.MONGO_URL = 'mongodb://' + mongoUsername + ':' + mongoPassword + '@ds163769.mlab.com:63769/v8-alba-mlab'
+// let mongoPassword = Meteor.settings.private.mlabpass
+// let mongoUsername = Meteor.settings.private.mlabuser
+// process.env.MONGO_URL = 'mongodb://' + mongoUsername + ':' + mongoPassword + '@ds163769.mlab.com:63769/v8-alba-mlab'
 
 import '../modules/index.js'
 import './seed.js'
