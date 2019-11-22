608a9b64a33be398b030587d1332926dd135b7c0
Kevin Ashworth <kevinashworth@yahoo.com>
2019-11-22 10:37:47 -0800

    I went way over on Algolia

--- packages/v8-alba/lib/server/main.js
+++ packages/v8-alba/lib/server/main.js
@@ -2,14 +2,14 @@ import '../modules/index.js'
 // import './seed.js'
 import './migrations.js'
 
-Meteor.call('getProcessEnvMongoUrl', function (err, results) {
-  if (err) {
-    console.error('getProcessEnvMongoUrl[main] error:', err)
-  }
-  if (results.indexOf('v8-alba-mlab') > 0) {
-    import './algolia.js'
-  }
-})
+// Meteor.call('getProcessEnvMongoUrl', function (err, results) {
+//   if (err) {
+//     console.error('getProcessEnvMongoUrl[main] error:', err)
+//   }
+//   if (results.indexOf('v8-alba-mlab') > 0) {
+//     import './algolia.js'
+//   }
+// })
 
 import Contacts from '../modules/contacts/collection.js'
 
