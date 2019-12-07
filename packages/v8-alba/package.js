d59181285dd0a1f594513f8696d40dee6c375d09
Kevin Ashworth <kevinashworth@yahoo.com>
2019-12-06 18:34:56 -0800

    Clean up stylesheets
    Order like in file folder, for visual convenience. Don't think load order matters.
    
    Move to a new folder (to be deleted?) those scss files that are now unused.

--- packages/v8-alba/package.js
+++ packages/v8-alba/package.js
@@ -14,14 +14,13 @@ Package.onUse(function (api) {
     'percolate:migrations'
   ]);
 
-  // api.addFiles('lib/stylesheets/scss/style.scss', 'client');
-  api.addFiles('lib/stylesheets/simple-line-icons.scss', 'client');
+  api.addFiles('lib/stylesheets/ContactsVirtualizedList.css', 'client');
   api.addFiles('lib/stylesheets/react-bootstrap-table-all.min.css', 'client');
-  api.addFiles('lib/stylesheets/spinner.scss', 'client');
   api.addFiles('lib/stylesheets/react-select.min.css', 'client');
   api.addFiles('lib/stylesheets/react-virtualized-select-styles.css', 'client');
   api.addFiles('lib/stylesheets/react-virtualized-styles.css', 'client');
-  api.addFiles('lib/stylesheets/ContactsVirtualizedList.css', 'client');
+  api.addFiles('lib/stylesheets/simple-line-icons.scss', 'client');
+  api.addFiles('lib/stylesheets/spinner.scss', 'client');
 
   api.addFiles([
     'lib/stylesheets/alba-2.0.9/react-perfect-scrollbar-styles.css',
