8a93185a65d3ada2aad30c620fc10f52e5750e38
Kevin Ashworth <kevinashworth@yahoo.com>
2019-12-13 15:49:19 -0800

    Make same changes in PastProjects
    But don't updatedAt

--- packages/v8-alba/lib/modules/past-projects/callbacks/offices.js
+++ packages/v8-alba/lib/modules/past-projects/callbacks/offices.js
@@ -53,70 +53,150 @@ So for each of the pastProject.offices we update office.pastProjects of the Offi
 //   })
 // }
 
-export function PastProjectEditUpdateOfficeBefore (data, { document, originalDocument }) {
-  const oldOffice = originalDocument.castingOfficeId
-  const newOffice = document.castingOfficeId
-  let adding = false
-  if (newOffice && !oldOffice) {
-    adding = true
-  }
-  let removing = false
-  if (oldOffice && !newOffice) {
-    // this is an office getting removed from the project, so we also need to remove the project from that office
-    removing = true
-  }
-  let replacing = false
-  if (newOffice && oldOffice && oldOffice !== newOffice) {
-    // this is one office replacing another on the project
-    removing = false
-    replacing = true
-  }
-  let itmightnotbethereforsomereason = false
-  if (newOffice === oldOffice) {
-    console.info('itmightnotbethereforsomereason')
-    itmightnotbethereforsomereason = true
-  }
+// export function PastProjectEditUpdateOfficeBefore (data, { document, originalDocument }) {
+//   const oldOffice = originalDocument.castingOfficeId
+//   const newOffice = document.castingOfficeId
+//   let adding = false
+//   if (newOffice && !oldOffice) {
+//     adding = true
+//   }
+//   let removing = false
+//   if (oldOffice && !newOffice) {
+//     // this is an office getting removed from the project, so we also need to remove the project from that office
+//     removing = true
+//   }
+//   let replacing = false
+//   if (newOffice && oldOffice && oldOffice !== newOffice) {
+//     // this is one office replacing another on the project
+//     removing = false
+//     replacing = true
+//   }
+//   let itmightnotbethereforsomereason = false
+//   if (newOffice === oldOffice) {
+//     console.info('itmightnotbethereforsomereason')
+//     itmightnotbethereforsomereason = true
+//   }
+//
+//   let office = null
+//   let pastProjects = null
+//   if (adding) {
+//     office = Offices.findOne(newOffice) // TODO: error handling
+//     pastProjects = office && office.pastProjects
+//   }
+//   if (removing || replacing || itmightnotbethereforsomereason) {
+//     office = Offices.findOne(oldOffice) // TODO: error handling
+//     pastProjects = office && office.pastProjects
+//   }
+//   if (!office) {
+//     console.log('findOne didn\'t find one office!')
+//     return
+//   }
+//   if (!isEmptyValue(pastProjects)) {
+//     if (itmightnotbethereforsomereason) {
+//       if (_.indexOf(pastProjects, { projectId: document._id }) < 0) {
+//         replacing = true
+//         console.info('itisnotthereforsomereason')
+//       }
+//     }
+//     _.remove(pastProjects, function (p) {
+//       return p.projectId === document._id
+//     })
+//     if (adding || replacing) {
+//       pastProjects.push({ projectId: document._id })
+//     }
+//     Connectors.update(Offices, office._id, {
+//       $set: {
+//         pastProjects: pastProjects,
+//         updatedAt: new Date()
+//       }
+//     })
+//   } else if (adding || itmightnotbethereforsomereason) { // means we are adding to empty (or null) projects
+//     pastProjects = [{ projectId: document._id }]
+//     Connectors.update(Offices, office._id, {
+//       $set: {
+//         pastProjects: pastProjects,
+//         updatedAt: new Date()
+//       }
+//     })
+//   }
+// }
 
-  let office = null
-  let pastProjects = null
-  if (adding) {
-    office = Offices.findOne(newOffice) // TODO: error handling
-    pastProjects = office && office.pastProjects
-  }
-  if (removing || replacing || itmightnotbethereforsomereason) {
-    office = Offices.findOne(oldOffice) // TODO: error handling
-    pastProjects = office && office.pastProjects
-  }
-  if (!office) {
-    console.log('findOne didn\'t find one office!')
+export function PastProjectEditUpdateOfficeBefore (data, { document, originalDocument }) {
+  console.group('PastProjectEditUpdateOfficeBefore:')
+
+  const oldOfficeId = originalDocument.castingOfficeId
+  const newOfficeId = document.castingOfficeId
+
+  if (newOfficeId === oldOfficeId) {
     return
   }
-  if (!isEmptyValue(pastProjects)) {
-    if (itmightnotbethereforsomereason) {
-      if (_.indexOf(pastProjects, { projectId: document._id }) < 0) {
-        replacing = true
-        console.info('itisnotthereforsomereason')
-      }
-    }
-    _.remove(pastProjects, function (p) {
-      return p.projectId === document._id
-    })
-    if (adding || replacing) {
-      pastProjects.push({ projectId: document._id })
-    }
-    Connectors.update(Offices, office._id, {
+
+  let addingToNewOffice = false
+  let removingFromOldOffice = false
+  if (newOfficeId && !oldOfficeId) {
+    addingToNewOffice = true
+  }
+  if (oldOfficeId && !newOfficeId) {
+    removingFromOldOffice = true
+  }
+  if ((newOfficeId && oldOfficeId) && (oldOfficeId !== newOfficeId)) {
+    addingToNewOffice = true
+    removingFromOldOffice = true
+  }
+
+  console.log('oldOfficeId:', oldOfficeId)
+  console.log('newOfficeId:', newOfficeId)
+  console.log('removingFromOldOffice:', removingFromOldOffice)
+  console.log('addingToNewOffice:', addingToNewOffice)
+
+  if (addingToNewOffice) {
+    let newOffice = Promise.await(Offices.findOne(newOfficeId)) // TODO: error handling
+    let currentNewOfficePastProjects = newOffice && newOffice.pastProjects
+    // add this pastProject to the new office's pastProjects,
+    // avoiding any potential duplication in case of database being slightly off
+    // TODO: _.unionWith is best?
+    const updatedNewOfficePastProjects = _.unionWith([{
+        projectId: document._id,
+        projectTitle: document.projectTitle
+      }], currentNewOfficePastProjects, _.isEqual
+    )
+
+    console.log('about to update', newOfficeId, 'with', updatedNewOfficePastProjects)
+
+    Connectors.update(Offices, newOfficeId, {
       $set: {
-        pastProjects: pastProjects,
-        updatedAt: new Date()
-      }
-    })
-  } else if (adding || itmightnotbethereforsomereason) { // means we are adding to empty (or null) projects
-    pastProjects = [{ projectId: document._id }]
-    Connectors.update(Offices, office._id, {
-      $set: {
-        pastProjects: pastProjects,
-        updatedAt: new Date()
-      }
+        pastProjects: updatedNewOfficePastProjects,
+        // updatedAt: new Date() // 2019-12-13: don't want to modify ancient past projects' dates
+        }
     })
   }
+  if (removingFromOldOffice) {
+    let oldOffice = Promise.await(Offices.findOne(oldOfficeId)) // TODO: error handling
+    let oldOfficePastProjects = oldOffice && oldOffice.pastProjects
+
+    console.log('[KA] oldOfficePastProjects:', oldOfficePastProjects)
+
+    _.remove(oldOfficePastProjects, function (p) {
+      return p.projectId === document._id
+    })
+
+    console.log('[KA] _removed and now:', oldOfficePastProjects)
+
+    if (isEmptyValue(oldOfficePastProjects)) {
+      Connectors.update(Offices, oldOfficeId, {
+        $unset: {
+          pastProjects: 1
+        }
+      })
+    } else {
+      Connectors.update(Offices, oldOfficeId, {
+        $set: {
+          pastProjects: oldOfficePastProjects,
+          // updatedAt: new Date() // 2019-12-13: don't want to modify ancient pastProjects' dates
+        }
+      })
+    }
+  }
+
+  console.groupEnd()
 }
