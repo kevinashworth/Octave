ac4c1510cb15698d0406b1acd8650af76fd4b3f3
Kevin Ashworth <kevinashworth@yahoo.com>
2020-01-01 11:20:14 -0800

    Remove now-unused CSS
    (The `virtualized` package needed `react-select` styles from earlier version, now we can remove that, too.)

--- packages/v8-alba/package.js
+++ packages/v8-alba/package.js
@@ -16,8 +16,6 @@ Package.onUse(function (api) {
 
   api.addFiles('lib/stylesheets/ContactsVirtualizedList.css', 'client');
   api.addFiles('lib/stylesheets/react-bootstrap-table-all.min.css', 'client');
-  api.addFiles('lib/stylesheets/react-select.min.css', 'client');
-  api.addFiles('lib/stylesheets/react-virtualized-select-styles.css', 'client');
   api.addFiles('lib/stylesheets/react-virtualized-styles.css', 'client');
   api.addFiles('lib/stylesheets/simple-line-icons.scss', 'client');
   api.addFiles('lib/stylesheets/spinner.scss', 'client');
