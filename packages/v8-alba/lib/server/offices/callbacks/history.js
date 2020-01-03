a8f4620c46899f36791504fd7a34bcc881166caf
Kevin Ashworth <kevinashworth@yahoo.com>
2020-01-03 08:15:52 -0800

    This is my best diff. For now.

--- packages/v8-alba/lib/server/offices/callbacks/history.js
+++ packages/v8-alba/lib/server/offices/callbacks/history.js
@@ -10,6 +10,7 @@ const isNullBlankOrUndefined = (o) => {
  * @param  {Object} base   Object to compare with
  * @param  {Object} ignoreBlanks Will not include properties whose value is null, undefined, etc.
  * @return {Object}        Return a new object who represent the diff
+ * See https://gist.github.com/Yimiprod/7ee176597fef230d1451#gistcomment-3102327
  */
 const objectDifference = (object, base, ignoreBlanks = true) => {
   if (!_.isObject(object) || _.isDate(object)) return object // special case dates
@@ -24,13 +25,10 @@ const objectDifference = (object, base, ignoreBlanks = true) => {
 
 export function OfficeEditUpdateHistoryBefore (data, { document, originalDocument }) {
   var objectId = document._id
-  var diff = objectDifference(data, originalDocument)
+  var diff1 = objectDifference(data, originalDocument)
 
-  if (diff) {
-    console.log('Here is the diff for', objectId, ':', diff)
+  if (diff1) {
+    console.log(`[KA] ${objectId} diff1:`, diff1)
+    console.log(`[KA] ${objectId} diff1 stringify:`, JSON.stringify(diff1))
   }
-
-  // console.log('data:', data)
-  // console.log('document:', document)
-  // console.log('originalDocument:', originalDocument)
 }
