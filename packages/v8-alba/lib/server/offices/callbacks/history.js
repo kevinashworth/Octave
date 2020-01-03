ed90ae5bde4ff09f1985aac4c548c05c74d86c1f
Kevin Ashworth <kevinashworth@yahoo.com>
2020-01-03 09:59:42 -0800

    I am storing basic diffs now. Format is this:
    {
    	"_id" : "YcRdgf2rNfZoYkbn7",
    	"changes" : [
    		{
    			"Fri Jan 03 2020 09:55:35 GMT-0800 (PST)" : {
    				"contacts" : [
    					{
    						"contactId" : "CzXrFuJCuoRbXvY8J",
    						"contactName" : "Testing Test",
    						"contactTitle" : "Intern"
    					}
    				],
    				"addresses" : [
    					{
    
    					}
    				]
    			}
    		},
    		{
    			"Fri Jan 03 2020 09:56:12 GMT-0800 (PST)" : {
    				"addresses" : [
    					{
    						"street2" : "Stage 4, 555th Floor"
    					}
    				]
    			}
    		}
    	],
    	"collectionName" : "Offices"
    }
    {
    	"_id" : "v4vAkMJ2EyqMmFKGw",
    	"changes" : [
    		{
    			"Fri Jan 03 2020 09:56:38 GMT-0800 (PST)" : {
    				"body" : "I'm loving it!",
    				"htmlBody" : "<p>I'm loving it!</p>\n"
    			}
    		}
    	],
    	"collectionName" : "Offices"
    }

--- packages/v8-alba/lib/server/offices/callbacks/history.js
+++ packages/v8-alba/lib/server/offices/callbacks/history.js
@@ -1,4 +1,9 @@
+import { Promise } from 'meteor/promise'
+import { Connectors } from 'meteor/vulcan:core'
 import _ from 'lodash'
+import Histories from '../../../modules/history/collection.js'
+
+// See https://gist.github.com/Yimiprod/7ee176597fef230d1451#gistcomment-3102327
 
 const isNullBlankOrUndefined = (o) => {
   return (typeof o === 'undefined' || o == null || o === '')
@@ -10,7 +15,6 @@ const isNullBlankOrUndefined = (o) => {
  * @param  {Object} base   Object to compare with
  * @param  {Object} ignoreBlanks Will not include properties whose value is null, undefined, etc.
  * @return {Object}        Return a new object who represent the diff
- * See https://gist.github.com/Yimiprod/7ee176597fef230d1451#gistcomment-3102327
  */
 const objectDifference = (object, base, ignoreBlanks = true) => {
   if (!_.isObject(object) || _.isDate(object)) return object // special case dates
@@ -23,12 +27,26 @@ const objectDifference = (object, base, ignoreBlanks = true) => {
   });
 }
 
-export function OfficeEditUpdateHistoryBefore (data, { document, originalDocument }) {
-  var objectId = document._id
-  var diff1 = objectDifference(data, originalDocument)
+export function OfficeEditUpdateHistoryBefore (data, { originalDocument }) {
+  var objectId = originalDocument._id
+  var diff = objectDifference(data, originalDocument)
+  // console.log(`[KA] ${objectId} diff:`, diff)
 
-  if (diff1) {
-    console.log(`[KA] ${objectId} diff1:`, diff1)
-    console.log(`[KA] ${objectId} diff1 stringify:`, JSON.stringify(diff1))
+  // updatedAt will always be there (right?), so only record history when more than one key in the diff object
+  if (Object.keys(diff).length > 1) {
+    const { updatedAt, ...rest } = diff
+    Promise.await(
+      Connectors.update(Histories, objectId,
+        {
+          $set: { collectionName: 'Offices'},
+          $addToSet: {
+            changes: {
+              [updatedAt]: { ...rest }
+            }
+          }
+        },
+        { upsert: true }
+      )
+    )
   }
 }
