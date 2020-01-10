a6de888b891554bf6f6bb432c523a23b007a2cef
Kevin Ashworth <kevinashworth@yahoo.com>
2020-01-10 14:57:19 -0800

    Refactor CommentsThread to avoid warning "Cannot update during an existing state transition (such as within `render`). Render methods should be a pure function of props and state."

--- packages/v8-alba/lib/components/comments/CommentsThread.jsx
+++ packages/v8-alba/lib/components/comments/CommentsThread.jsx
@@ -1,39 +1,65 @@
 import { Components, registerComponent, Utils, withCurrentUser, withMulti } from 'meteor/vulcan:core'
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Card, CardBody, CardHeader } from 'reactstrap'
 import _ from 'lodash'
 import pluralize from 'pluralize'
 import Comments from '../../modules/comments/collection.js'
 
-const CommentsThread = (props, context) => {
-  const { loading, terms: { collectionName, objectId }, results, totalCount, currentUser, callbackFromSingle } = props
+class CommentsThread extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      commentsHeader: 'Comments'
+    }
+  }
 
-  if (loading) {
-    return <div className='posts-comments-thread'><Components.Loading /></div>
-  } else {
-    const commentsHeader = pluralize('Comment', totalCount, true)
-    callbackFromSingle(commentsHeader)
-    const resultsClone = _.map(results, _.clone) // we don't want to modify the objects we got from props
-    const nestedComments = Utils.unflatten(resultsClone, { idProperty: '_id', parentIdProperty: 'parentCommentId' })
+  // we need both these (nearly identical) functions
+  componentDidMount () {
+    const { totalCount, callbackFromSingle } = this.props
+    if (totalCount) {
+      const commentsHeader = pluralize('Comment', totalCount, true)
+      this.setState({ commentsHeader })
+      callbackFromSingle(commentsHeader)
+    }
+  }
+  componentDidUpdate (prevProps) {
+    const { totalCount, callbackFromSingle } = this.props
+    if (prevProps.totalCount !== totalCount) {
+      const commentsHeader = pluralize('Comment', totalCount, true)
+      this.setState({ commentsHeader })
+      callbackFromSingle(commentsHeader)
+    }
+  }
 
-    return (
-      <Card>
-        <CardHeader className='class-accent-list'>{commentsHeader}</CardHeader>
-        <CardBody>
-          <Components.CommentsList currentUser={currentUser} comments={nestedComments} commentCount={totalCount} />
-        </CardBody>
-        <CardBody>
-          {currentUser
-            ? <Components.CommentsNewForm
-                collectionName={collectionName}
-                objectId={objectId}
-                type='comment'
-              />
-            : null }
-        </CardBody>
-      </Card>
-    )
+  render () {
+    const { loading, terms: { collectionName, objectId }, results, totalCount, currentUser } = this.props
+
+    if (loading) {
+      return <Components.Loading />
+    } else {
+      const resultsClone = _.map(results, _.clone) // we don't want to modify the objects we got from props
+      const nestedComments = Utils.unflatten(resultsClone, { idProperty: '_id', parentIdProperty: 'parentCommentId' })
+
+      return (
+        <Card>
+          <CardHeader className='class-accent-list'>{this.state.commentsHeader}</CardHeader>
+          <CardBody>
+            <Components.CommentsList currentUser={currentUser} comments={nestedComments} commentCount={totalCount} />
+          </CardBody>
+          <CardBody>
+            {currentUser
+              ? <Components.CommentsNewForm
+                  collectionName={collectionName}
+                  objectId={objectId}
+                  type='comment'
+                />
+              : null }
+          </CardBody>
+        </Card>
+      )
+    }
+
   }
 }
 
