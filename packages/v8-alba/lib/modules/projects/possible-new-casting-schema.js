57194d09650b6c5d6ca977f907b285e5cc6e2b83
Kevin Ashworth <kevinashworth@yahoo.com>
2020-01-25 14:23:07 -0800

    Here is a db record showing how a project with multiple casting offices will look

---
+++ packages/v8-alba/lib/modules/projects/possible-new-casting-schema.js
@@ -0,0 +1,55 @@
+/* eslint-disable */
+export default {
+  "_id": "2fJpT9D7QZKnzAJf6",
+  "website": "http://www.fox.com/empire",
+  "projectTitle": "Empire",
+  "projectType": "TV One Hour",
+  "union": "SAG-AFTRA",
+  "network": "FOX",
+  "status": "Casting",
+  "notes": "Season 6 will be the last. Season 6 production dates: 07/17/2019 - 03/24/2020\n\nShoots and primarily casts in Chicago.",
+  "season": "6",
+  "order": "18",
+  "userId": "22nAExKyDXNhytfx9",
+  "createdAt": {
+    "$date": "2018-07-22T01:02:16.360Z"
+  },
+  "updatedAt": {
+    "$date": "2019-12-02T17:05:39.593Z"
+  },
+  "slug": "empire",
+  "htmlSummary": "<p><strong>SUMMARY:</strong> A powerful drama about a family dynasty set within the glamorous and sometimes dangerous world of hip-hop music, the show revolves around the Lyons and their media company, Empire Entertainment.</p>\n",
+  "summary": "A powerful drama about a family dynasty set within the glamorous and sometimes dangerous world of hip-hop music, the show revolves around the Lyons and their media company, Empire Entertainment.",
+  "castingOffices": [
+    {
+      "castingLocation": "Los Angeles",
+      "castingOfficeId": "RSeoiDy7QudYFY7nx",
+      "contacts": [
+        {
+          "contactId": "RDLDBi9L7pqBdgJKG",
+          "contactName": "Leah Daniels-Butler",
+          "contactTitle": "Casting Director"
+        },
+        {
+          "contactId": "Dmg5t4b2kCWQ7xPeM",
+          "contactName": "Jill Uyeda",
+          "contactTitle": "Casting Director"
+        }
+      ]
+    },
+    {
+      "castingLocation": "Chicago",
+      "castingOfficeId": "RSeoiDy7QudYFY7nx",
+      "contacts": [
+        {
+          "contactId": "7qrw7Yr5MjCRLEqb6",
+          "contactName": "Claire Simon",
+          "contactTitle": "Casting Director"
+        }
+      ]
+    }
+  ],
+  "htmlNotes": "<p><strong>NOTES:</strong> Season 6 will be the last. Season 6 production dates: 07/17/2019 - 03/24/2020</p>\n<p>Shoots and primarily casts in Chicago.</p>\n",
+  "platformType": "Broadcast",
+  "sortTitle": "empire"
+}
