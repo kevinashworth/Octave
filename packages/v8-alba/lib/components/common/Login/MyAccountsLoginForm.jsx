1d75f6445a8c19366e986148511d6f1ffa48908b
Kevin Ashworth <kevinashworth@yahoo.com>
2020-02-08 08:11:49 -0800

    Committing some commented-out, just in case

--- packages/v8-alba/lib/components/common/Login/MyAccountsLoginForm.jsx
+++ packages/v8-alba/lib/components/common/Login/MyAccountsLoginForm.jsx
@@ -1,12 +1,39 @@
 import { Components, replaceComponent } from 'meteor/vulcan:core'
 import React, { Component } from 'react'
 
+// let browserHistory
+// try {
+//   browserHistory = require('react-router').browserHistory
+// } catch(e) {
+//   // swallow errors
+// }
+
+// const onSignedInHook = (redirect) => {
+//   if (Meteor.isClient) {
+//     if (window.history) {
+//       Meteor.setTimeout(() => {
+//         if (browserHistory) {
+//           browserHistory.push(redirect)
+//         } else {
+//           window.history.pushState( {} , 'redirect', redirect )
+//         }
+//       }, 100)
+//     }
+//   }
+// }
+
+// const options = {
+//   homeRoutePath: '/latest',
+//   onSignedInHook: onSignedInHook('/dashboard'),
+//   requireEmailVerification: true
+// }
+
 export class MyAccountsLoginForm extends Component {
   componentDidMount() {
     document.getElementById('facebook').innerHTML = '<span>Facebook</span>'
     document.getElementById('github').innerHTML = '<span>Github</span>'
   }
-  
+
   render () {
     return (
       <Components.AccountsStateSwitcher {...this.props} />
