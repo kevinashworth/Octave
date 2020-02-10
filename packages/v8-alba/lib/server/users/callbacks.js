2ab38a75bd2b8596d8a5cf20e876f91f19c912dd
Kevin Ashworth <kevinashworth@yahoo.com>
2020-02-10 06:37:41 -0800

    An attempt at making handles work upon updates. WIP

--- packages/v8-alba/lib/server/users/callbacks.js
+++ packages/v8-alba/lib/server/users/callbacks.js
@@ -1,8 +1,26 @@
 import { Accounts } from 'meteor/accounts-base'
-import { addCallback } from 'meteor/vulcan:core'
+import { addCallback, Connectors } from 'meteor/vulcan:core'
+import Users from 'meteor/vulcan:users'
 
 async function sendVerificationEmail ({ document: user }) {
   Accounts.sendVerificationEmail(user._id);
 }
 
+async function mapEmails ({ document: user }) {
+  if (user.emails && user.emails[0]) {
+    const [...handles] = user.emails
+    const emailAddress = user.emails[0].address
+    const emailVerified = user.emails[0].verified
+    await Connectors.update(Users, user._id, {
+        $set: {
+          handles,
+          emailAddress,
+          emailVerified
+        }
+      })
+  }
+
+}
+
 addCallback('user.create.async', sendVerificationEmail)
+addCallback('user.update.async', mapEmails)
