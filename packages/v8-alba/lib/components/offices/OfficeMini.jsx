f2f5542aee9bf3fc6359df6f71a579e995b7ab58
Kevin Ashworth <kevinashworth@yahoo.com>
2020-02-17 22:12:44 -0800

    OfficeMini --> PureComponent saves some re-renders like "props did not change but the props object did change"

--- packages/v8-alba/lib/components/offices/OfficeMini.jsx
+++ packages/v8-alba/lib/components/offices/OfficeMini.jsx
@@ -1,6 +1,6 @@
 import { Components, registerComponent, withCurrentUser, withSingle } from 'meteor/vulcan:core'
 import { FormattedMessage } from 'meteor/vulcan:i18n'
-import React from 'react'
+import React, { PureComponent } from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { Card, CardBody, CardHeader } from 'reactstrap'
@@ -8,33 +8,37 @@ import Markup from 'interweave'
 import { createdFormattedAddress } from '../../modules/helpers.js'
 import Offices from '../../modules/offices/collection.js'
 
-const OfficeMini = (props) => {
-  if (props.loading) {
-    return <Components.Loading />
-  }
-  if (!props.document || !props.documentId) {
+class OfficeMini extends PureComponent {
+  render () {
+    const { document: office, documentId, loading, officeName } = this.props
+
+    if (loading) {
+      return <Components.Loading />
+    }
+    if (!document || !documentId) {
+      return (
+        <Card className='card-accent-primary' style={{minWidth: '8rem', maxWidth: '18rem'}}>
+          <FormattedMessage id='app.missing_document' />
+        </Card>
+      )
+    }
     return (
       <Card className='card-accent-primary' style={{minWidth: '8rem', maxWidth: '18rem'}}>
-        <FormattedMessage id='app.missing_document' />
+        <CardHeader tag='h6'>
+          <Link to={`/offices/${office._id}/${office.slug}`}>
+            {officeName ? officeName : office.displayName}
+          </Link>
+        </CardHeader>
+        {office.addresses && office.addresses.map(address => {
+          return (
+            <CardBody key={address.street1 + address.street2}>
+              <Markup content={createdFormattedAddress(address)}/>
+            </CardBody>
+          )
+        })}
       </Card>
     )
   }
-
-  const office = props.document
-  const officeName = props.officeName ? props.officeName : office.displayName
-
-  return (
-    <Card className='card-accent-primary' style={{minWidth: '8rem', maxWidth: '18rem'}}>
-      <CardHeader tag='h6'><Link to={`/offices/${office._id}/${office.slug}`}>{officeName}</Link></CardHeader>
-      {office.addresses && office.addresses.map(address => {
-        return (
-          <CardBody key={address.street1 + address.street2}>
-            <Markup content={createdFormattedAddress(address)}/>
-          </CardBody>
-        )
-      })}
-    </Card>
-  )
 }
 
 OfficeMini.propTypes = {
