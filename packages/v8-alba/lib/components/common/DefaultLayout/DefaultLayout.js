01413acdd96319308a4e1e09951c7f5ad5faf5f7
Kevin Ashworth <kevinashworth@yahoo.com>
2020-03-01 18:22:57 -0800

    Add a block 8 favicon

--- packages/v8-alba/lib/components/common/DefaultLayout/DefaultLayout.js
+++ packages/v8-alba/lib/components/common/DefaultLayout/DefaultLayout.js
@@ -1,6 +1,7 @@
 import { Components, replaceComponent, withCurrentUser } from 'meteor/vulcan:core';
 import Users from 'meteor/vulcan:users';
 import React, { Component } from 'react';
+import { Helmet } from 'react-helmet';
 import Media from 'react-media';
 import { Container } from 'reactstrap';
 import {
@@ -37,6 +38,15 @@ class DefaultLayout extends Component {
 
     return (
       <div className="app">
+        <Helmet>
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
+          <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#20a8d8" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+        </Helmet>
         <AppHeader fixed>
           <DefaultHeader />
         </AppHeader>
