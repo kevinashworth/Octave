72754e1960129dacb9cbaf5ec94f36fd09fbc154
Kevin Ashworth <kevinashworth@yahoo.com>
2020-03-09 16:32:15 -0700

    Removed EmailEditForm, moved EmailDetail

--- packages/v8-alba/lib/components/users/EmailDetail.jsx
+++ packages/v8-alba/lib/components/users/EmailDetail.jsx
@@ -2,24 +2,10 @@ import { Components, registerComponent, withMessages } from 'meteor/vulcan:core'
 import { FormattedMessage } from 'meteor/vulcan:i18n'
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import { Button, ButtonGroup, Card, CardBody, Col, Row } from 'reactstrap'
+import { Card, CardBody, Col, Row } from 'reactstrap'
 
 class EmailDetail extends PureComponent {
-  constructor (props) {
-    super(props)
-    this.emailEditSuccessCallback = this.emailEditSuccessCallback.bind(this)
-    this.sendVerificationEmail = this.sendVerificationEmail.bind(this)
-  }
-
-  emailEditSuccessCallback ({ address }) {
-    this.props.flash({
-      id: 'users.add_email_success',
-      properties: { address },
-      type: 'success'
-    })
-  }
-
-  sendVerificationEmail () {
+  sendVerificationEmail = () => {
     Meteor.call(
       'sendVerificationEmail',
       {
@@ -54,14 +40,6 @@ class EmailDetail extends PureComponent {
             <Col xs>
               <strong>{user.emailAddress}&nbsp;</strong>
               {user.emailPrimary && <span className='text-success'>&nbsp;(<FormattedMessage id='users.primary_email' />)&nbsp;</span> }
-            </Col>
-            <Col xs='auto'>
-              <ButtonGroup>
-                <Components.ModalTrigger
-                component={<Button color='ghost-secondary'><i className='fa fa-pencil-square-o' /></Button>}>
-                  <Components.EmailEditForm user={user} successCallback={this.emailEditSuccessCallback} />
-                </Components.ModalTrigger>
-              </ButtonGroup>
             </Col>
           </Row>
           <Row>
