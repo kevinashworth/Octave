cfeaf86d3be3206258530244d4be0c13a21028a4
Kevin Ashworth <kevinashworth@yahoo.com>
2020-05-05 07:06:54 -0700

    Change to Date in LineChartLarge
    - x: moment(stat.date).format('D MMM YY') --> x: stat.date
    - also remove useMemo, as I don't see any performance improvement with it
    - clean up
    - seems to work right in Firefox now, too

--- packages/v8-alba/lib/components/common/Dashboard/LineChartLarge.jsx
+++ packages/v8-alba/lib/components/common/Dashboard/LineChartLarge.jsx
@@ -1,5 +1,5 @@
 import { Components, registerComponent } from 'meteor/vulcan:core'
-import React, { useMemo, useState } from 'react'
+import React, { useState } from 'react'
 import { Line } from 'react-chartjs-2'
 import {
   Button,
@@ -34,7 +34,7 @@ const Item = styled.li`
 
 const xy = (stat) => {
   return {
-    x: moment(stat.date).format('D MMM YY'),
+    x: stat.date,
     y: stat.quantity
   }
 }
@@ -51,80 +51,53 @@ function LineChartLarge (props) {
   const [timeframe, setTimeframe] = useState(3)
   const { loading, theStats } = props
 
-  const xyEpisodics = useMemo(
-    () => theStats.episodics.map(stat => xy(stat)),
-    [theStats]
-  )
-  const xyFeatures = useMemo(
-    () => theStats.features.map(stat => xy(stat)),
-    [theStats]
-  )
-  const xyPilots = useMemo(
-    () => theStats.pilots.map(stat => xy(stat)),
-    [theStats]
-  )
-  const xyOthers = useMemo(
-    () => theStats.others.map(stat => xy(stat)),
-    [theStats]
-  )
+  const xyEpisodics = theStats.episodics.map(stat => xy(stat))
+  const xyFeatures = theStats.features.map(stat => xy(stat))
+  const xyPilots = theStats.pilots.map(stat => xy(stat))
+  const xyOthers = theStats.others.map(stat => xy(stat))
 
-  const theEpisodics = useMemo(
-    () => getRecent(xyEpisodics, timeframe),
-    [xyEpisodics, timeframe]
-  )
-  const theFeatures = useMemo(
-    () => getRecent(xyFeatures, timeframe),
-    [xyFeatures, timeframe]
-  )
-  const thePilots = useMemo(
-    () => getRecent(xyPilots, timeframe),
-    [xyPilots, timeframe]
-  )
-  const theOthers = useMemo(
-    () => getRecent(xyOthers, timeframe),
-    [xyOthers, timeframe]
-  )
+  const theEpisodics = getRecent(xyEpisodics, timeframe)
+  const theFeatures = getRecent(xyFeatures, timeframe)
+  const thePilots = getRecent(xyPilots, timeframe)
+  const theOthers = getRecent(xyOthers, timeframe)
 
   const datasetProps = {
     backgroundColor: 'transparent',
     pointHoverBackgroundColor: '#fff',
     borderWidth: 2
   }
-  const mainChart = useMemo(
-    () => ({
-      datasets: [
-        {
-          label: 'Episodics',
-          xAxisID: 'x-axis-episodics',
-          borderColor: brandColors['success'],
-          data: theEpisodics,
-          ...datasetProps
-        },
-        {
-          label: 'Features',
-          xAxisID: 'x-axis-features',
-          borderColor: brandColors['info'],
-          data: theFeatures,
-          ...datasetProps
-        },
-        {
-          label: 'Pilots',
-          xAxisID: 'x-axis-pilots',
-          borderColor: brandColors['warning'],
-          data: thePilots,
-          ...datasetProps
-        },
-        {
-          label: 'Others',
-          xAxisID: 'x-axis-others',
-          borderColor: brandColors['danger'],
-          data: theOthers,
-          ...datasetProps
-        }
-      ]
-    }),
-    [theEpisodics, theFeatures, thePilots, theOthers, datasetProps]
-  )
+  const mainChart = {
+    datasets: [
+      {
+        label: 'Episodics',
+        xAxisID: 'x-axis-episodics',
+        borderColor: brandColors['success'],
+        data: theEpisodics,
+        ...datasetProps
+      },
+      {
+        label: 'Features',
+        xAxisID: 'x-axis-features',
+        borderColor: brandColors['info'],
+        data: theFeatures,
+        ...datasetProps
+      },
+      {
+        label: 'Pilots',
+        xAxisID: 'x-axis-pilots',
+        borderColor: brandColors['warning'],
+        data: thePilots,
+        ...datasetProps
+      },
+      {
+        label: 'Others',
+        xAxisID: 'x-axis-others',
+        borderColor: brandColors['danger'],
+        data: theOthers,
+        ...datasetProps
+      }
+    ]
+  }
 
   const xProps = {
     position: 'bottom',
@@ -183,52 +156,52 @@ function LineChartLarge (props) {
     return <Components.Loading />
   }
   return (
-      <Card>
-        <CardBody>
-          <Row>
-            <Col sm='5'>
-              <CardTitle className='mb-0'>Number of TV &amp; Film Projects Casting</CardTitle>
-            </Col>
-            <Col sm='7' className='d-none d-sm-inline-block'>
-              <ButtonToolbar className='float-right'>
-                <ButtonGroup>
-                  <Button outline color='secondary' onClick={() => setTimeframe(1)} active={timeframe === 1}>Month</Button>
-                  <Button outline color='secondary' onClick={() => setTimeframe(2)} active={timeframe === 2}>Year</Button>
-                  <Button outline color='secondary' onClick={() => setTimeframe(3)} active={timeframe === 3}>All</Button>
-                </ButtonGroup>
-              </ButtonToolbar>
-            </Col>
-          </Row>
-          <div className='chart-wrapper'>
-            <Line data={mainChart} height={400} options={mainChartOpts} />
-          </div>
-        </CardBody>
-        <CardFooter>
-          <List>
-            <Item>
-              <strong>Episodics</strong>
-              <div className='text-muted'>{theStats.episodics[theStats.episodics.length - 1].quantity} Currently Casting</div>
-              <Progress className='progress-xs mt-2' color='success' value='100' />
-            </Item>
-            <Item>
-              <strong>Features</strong>
-              <div className='text-muted'>{theStats.features[theStats.features.length - 1].quantity} Currently Casting</div>
-              <Progress className='progress-xs mt-2' color='info' value='100' />
-            </Item>
-            <Item>
-              <strong>Pilots</strong>
-              <div className='text-muted'>{theStats.pilots[theStats.pilots.length - 1].quantity} Currently Casting</div>
-              <Progress className='progress-xs mt-2' color='warning' value='100' />
-            </Item>
-            <Item>
-              <strong>Others</strong>
-              <div className='text-muted'>{theStats.others[theStats.others.length - 1].quantity} Currently Casting</div>
-              <Progress className='progress-xs mt-2' color='danger' value='100' />
-            </Item>
-          </List>
-        </CardFooter>
-      </Card>
-    )
+    <Card>
+      <CardBody>
+        <Row>
+          <Col sm='5'>
+            <CardTitle className='mb-0'>Number of TV &amp; Film Projects Casting</CardTitle>
+          </Col>
+          <Col sm='7' className='d-none d-sm-inline-block'>
+            <ButtonToolbar className='float-right'>
+              <ButtonGroup>
+                <Button outline color='secondary' onClick={() => setTimeframe(1)} active={timeframe === 1}>Month</Button>
+                <Button outline color='secondary' onClick={() => setTimeframe(2)} active={timeframe === 2}>Year</Button>
+                <Button outline color='secondary' onClick={() => setTimeframe(3)} active={timeframe === 3}>All</Button>
+              </ButtonGroup>
+            </ButtonToolbar>
+          </Col>
+        </Row>
+        <div className='chart-wrapper'>
+          <Line data={mainChart} height={400} options={mainChartOpts} />
+        </div>
+      </CardBody>
+      <CardFooter>
+        <List>
+          <Item>
+            <strong>Episodics</strong>
+            <div className='text-muted'>{theStats.episodics[theStats.episodics.length - 1].quantity} Currently Casting</div>
+            <Progress className='progress-xs mt-2' color='success' value='100' />
+          </Item>
+          <Item>
+            <strong>Features</strong>
+            <div className='text-muted'>{theStats.features[theStats.features.length - 1].quantity} Currently Casting</div>
+            <Progress className='progress-xs mt-2' color='info' value='100' />
+          </Item>
+          <Item>
+            <strong>Pilots</strong>
+            <div className='text-muted'>{theStats.pilots[theStats.pilots.length - 1].quantity} Currently Casting</div>
+            <Progress className='progress-xs mt-2' color='warning' value='100' />
+          </Item>
+          <Item>
+            <strong>Others</strong>
+            <div className='text-muted'>{theStats.others[theStats.others.length - 1].quantity} Currently Casting</div>
+            <Progress className='progress-xs mt-2' color='danger' value='100' />
+          </Item>
+        </List>
+      </CardFooter>
+    </Card>
+  )
 }
 
 registerComponent('LineChartLarge', LineChartLarge)
