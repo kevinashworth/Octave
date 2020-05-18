d7413fd5947764e7891af78a860f91a57521ac2e
Kevin Ashworth <kevinashworth@yahoo.com>
2020-05-18 12:09:20 -0700

    Tweak col order
    - make all one row, to order by breakpoint

--- packages/v8-alba/lib/components/common/Dashboard/Dashboard.js
+++ packages/v8-alba/lib/components/common/Dashboard/Dashboard.js
@@ -13,7 +13,7 @@ class Dashboard extends PureComponent {
       <div className='animated fadeIn'>
         <Components.HeadTags title='V8 Alba: Dashboard' />
         <Row className='mt-4'>
-          <Col xs='12' sm='6' lg='3'>
+          <Col xs='12' sm='6' lg='3' className='order-2 order-md-1'>
             <Components.LineChartSmall
               bgColor='success'
               theSmallStats={theStats.episodics}
@@ -22,7 +22,7 @@ class Dashboard extends PureComponent {
             />
           </Col>
 
-          <Col xs='12' sm='6' lg='3'>
+          <Col xs='12' sm='6' lg='3' className='order-3 order-md-2'>
             <Components.LineChartSmall
               bgColor='info'
               theSmallStats={theStats.features}
@@ -31,7 +31,7 @@ class Dashboard extends PureComponent {
             />
           </Col>
 
-          <Col xs='12' sm='6' lg='3'>
+          <Col xs='12' sm='6' lg='3' className='order-4 order-md-3'>
             <Components.LineChartSmall
               bgColor='warning'
               theSmallStats={theStats.pilots}
@@ -40,7 +40,7 @@ class Dashboard extends PureComponent {
             />
           </Col>
 
-          <Col xs='12' sm='6' lg='3'>
+          <Col xs='12' sm='6' lg='3' className='order-5 order-md-4'>
             <Components.LineChartSmall
               bgColor='danger'
               theSmallStats={theStats.others}
@@ -48,9 +48,8 @@ class Dashboard extends PureComponent {
               subtitle={'Last 12 months'}
             />
           </Col>
-        </Row>
-        <Row>
-          <Col>
+
+          <Col xs='12' className='order-1 order-md-5'>
             <Components.LineChartLarge theStats={theStats} />
           </Col>
         </Row>
