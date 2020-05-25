78434935fd43833ff785b2f165a68f76827d924d
Kevin Ashworth <kevinashworth@yahoo.com>
2020-05-24 18:43:03 -0700

    Rewrite LinkDetail

--- packages/v8-alba/lib/components/common/LinkDetail.jsx
+++ packages/v8-alba/lib/components/common/LinkDetail.jsx
@@ -1,19 +1,91 @@
 import { registerComponent } from 'meteor/vulcan:core'
-import React, { PureComponent } from 'react'
+import React from 'react'
+import { Button } from 'react-bootstrap'
 import PropTypes from 'prop-types'
-import { Button, CardLink } from 'reactstrap'
+import find from 'lodash/find'
 
-class LinkDetail extends PureComponent {
-  render () {
-    const { link } = this.props
-    return (
+let faBrandList = [
+  'behance', 'css3', 'dribbble', 'dropbox', 'facebook', 'flickr', 'foursquare', 'github',
+  'google-plus', 'html5', 'instagram', 'linkedin', 'openid', 'pinterest', 'reddit', 'spotify',
+  'stack-overflow', 'tumblr', 'twitter', 'vimeo', 'vine', 'vk', 'xing', 'yahoo', 'youtube'
+]
+faBrandList.concat([
+  'imdb', 'wikipedia'
+])
 
-      <Button className={`btn-${link.platformName.toLowerCase()} text-white`} key={link.profileLink}>
-        <span><CardLink href={link.profileLink} target='profilelinks'>{link.profileName}</CardLink></span>
-      </Button>
-
-    )
+const myBrandList = [
+  {
+    brand: 'cbs',
+    fa: 'fa-television'
+  },
+  {
+    brand: 'csa',
+    fa: 'fa-desktop'
+  },
+  {
+    brand: 'freeform',
+    fa: 'fa-television'
+  },
+  {
+    brand: 'hulu',
+    fa: 'fa-television'
+  },
+  {
+    brand: 'imdbpro',
+    fa: 'fa-imdb'
+  },
+  {
+    brand: 'netflix',
+    fa: 'fa-television'
+  },
+  {
+    brand: 'vudu',
+    fa: 'fa-television'
+  },
+  {
+    brand: 'website',
+    fa: 'fa-desktop'
   }
+]
+
+const getPlatform = (platformName) => {
+  let platform = platformName.toLowerCase()
+  platform = platform.replace(/\s+/g, '')
+  return platform
+}
+const buttonClass = (platformName) => {
+  const platform = getPlatform(platformName)
+  return `btn-${platform} btn-brand mr-1 mb-1`
+}
+const iconClass = (platformName) => {
+  const platform = getPlatform(platformName)
+  const theirBrand = faBrandList.indexOf(platform)
+  if (theirBrand > -1) {
+    return 'fa fa-' + platform
+  }
+  const brand = find(myBrandList, {brand: platform})
+  if (brand) {
+    return 'fa ' + brand.fa
+  } else {
+    return 'fa fa-star'
+  }
+}
+
+const LinkDetail = (props) => {
+  const { link } = props
+  const btn = buttonClass(link.platformName)
+  const icon = iconClass(link.platformName)
+
+  return (
+    <Button className={btn}>
+      <i className={icon} />
+      <span>
+        <a href={link.profileLink} target='profilelinks'>
+          {link.profileName}
+        </a>
+      </span>
+    </Button>
+  )
 }
 
 LinkDetail.propTypes = {
