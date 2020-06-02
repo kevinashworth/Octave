d179cd4d2d08db8fe7afe93327e7973a014468ea
Kevin Ashworth <kevinashworth@yahoo.com>
2020-06-02 08:27:18 -0700

    Improve Search and User - Lots of small but real Search changes here! - And card-title styling

--- packages/v8-alba/lib/components/common/DefaultLayout/DefaultHeader.js
+++ packages/v8-alba/lib/components/common/DefaultLayout/DefaultHeader.js
@@ -1,51 +1,34 @@
 import { Components } from 'meteor/vulcan:core'
-import React, { Component } from 'react'
+import React from 'react'
 // import { Dropdown, DropdownMenu, DropdownToggle, Nav } from 'reactstrap';
 import Dropdown from 'react-bootstrap/Dropdown'
 import Nav from 'react-bootstrap/Nav'
 import NavItem from 'react-bootstrap/NavItem'
 import NavLink from 'react-bootstrap/NavLink'
-import PropTypes from 'prop-types'
 
 import { AppNavbarBrand, AppSidebarToggler } from '@coreui/react'
 
-const propTypes = {
-  children: PropTypes.node
+const DefaultHeader = () => {
+  return (
+    <>
+      <AppSidebarToggler className='d-lg-none' display='md' mobile />
+      <AppNavbarBrand
+        full={{ src: '/img/V8.png', width: 60, height: 30, alt: 'Horiz' }}
+        minimized={{ src: '/favicon/favicon-32x32.png', width: 32, height: 32, alt: 'Square' }}
+      />
+      <AppSidebarToggler className='d-md-down-none' display='lg' />
+      <Nav className='ml-auto'>
+        <Dropdown as={NavItem} drop='left'>
+          <Dropdown.Toggle as={NavLink} id='dropdown-users-menu'>
+            <i className='fa fa-user-circle-o fa-lg' />
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Components.UsersMenu />
+          </Dropdown.Menu>
+        </Dropdown>
+      </Nav>
+    </>
+  )
 }
-
-const defaultProps = {}
-
-class DefaultHeader extends Component {
-  render () {
-    // eslint-disable-next-line
-    const { children, ...attributes } = this.props;
-
-    return (
-      <>
-        <AppSidebarToggler className='d-lg-none' display='md' mobile />
-        <AppNavbarBrand
-          full={{ src: '/img/V8.png', width: 60, height: 30, alt: 'Horiz' }}
-          minimized={{ src: '/favicon/favicon-32x32.png', width: 32, height: 32, alt: 'Square' }}
-        />
-        <AppSidebarToggler className='d-md-down-none' display='lg' />
-        <Nav className='ml-auto' navbar>
-          <Dropdown as={NavItem}>
-            <Dropdown.Toggle as={NavLink}>
-              <i className='icon-user font-2xl' />
-            </Dropdown.Toggle>
-            <Dropdown.Menu right>
-              <Components.UsersMenu />
-            </Dropdown.Menu>
-          </Dropdown>
-        </Nav>
-        {/* <AppAsideToggler className="d-md-down-none" />
-        <AppAsideToggler className="d-lg-none" mobile /> */}
-      </>
-    )
-  }
-}
-
-DefaultHeader.propTypes = propTypes
-DefaultHeader.defaultProps = defaultProps
 
 export default DefaultHeader
