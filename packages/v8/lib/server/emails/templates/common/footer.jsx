5e124d0eb89c83b001f848b487585644c39b4193
Kevin Ashworth <kevinashworth@yahoo.com>
2020-06-03 17:22:23 -0700

    clean up footer

--- packages/v8/lib/server/emails/templates/common/footer.jsx
+++ packages/v8/lib/server/emails/templates/common/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const footer = () => {
   return (
     <span>
-      <a href='//v8.the-4th-wall.com/'>V8</a> &copy; 2018-2020 SuperSecretSite, LLC<br />
+      <a href='//v8.the-4th-wall.com/'>V8</a> &copy; 2018-2020 SuperSite, LLC<br />
       Powered by <a href='//the-4th-wall.com/'>The 4th Wall</a>
     </span>
   )
