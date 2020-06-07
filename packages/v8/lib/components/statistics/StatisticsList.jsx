4599aab842bc98cf5005acfde0dcadb07fbf1cf7
Kevin Ashworth <kevinashworth@yahoo.com>
2020-06-06 18:09:58 -0700

    Improve StatisticsList - Separate StatisticsList and StatisticsEditForm into 2 distinct components - Show stats in columns by type - Use Fade (a Collapse-like option) - css 'tabular-nums' and improve (?) font stack

--- packages/v8/lib/components/statistics/StatisticsList.jsx
+++ packages/v8/lib/components/statistics/StatisticsList.jsx
@@ -1,32 +1,115 @@
 import { Components, registerComponent, withCurrentUser, withMulti } from 'meteor/vulcan:core'
-import React from 'react'
+import Users from 'meteor/vulcan:users'
+import React, { useState } from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
+import Col from 'react-bootstrap/Col'
+import Fade from 'react-bootstrap/Fade'
 import ListGroup from 'react-bootstrap/ListGroup'
+import Row from 'react-bootstrap/Row'
+import moment from 'moment'
 import Statistics from '../../modules/statistics/collection.js'
 
-const StatisticsList = ({ loading, loadingMore, loadMore, results = [], currentUser, count, totalCount }) => {
+const Item = ({ date, quantity}) => {
+  return (
+    <ListGroup.Item key={date}>
+      {moment(date).format('DD MMM YYYY')}: <strong>{quantity}</strong>
+    </ListGroup.Item>
+  )
+}
+
+const StatisticsList = (props) => {
+  const [open, setOpen] = useState(Array(4).fill(true))
+  const { currentUser, loading, results = [] } = props
   if (loading) {
     return <Components.Loading />
   }
-  const hasMore = results && (totalCount > results.length)
+  const theStats = results[0]
+  const toggle = (i) => {
+    const newArray = open.map((element, index) => {
+      return (index === i ? !element : element)
+    })
+    setOpen(newArray)
+  }
   return (
     <div className='animated fadeIn'>
       <Card>
         <Card.Header>
-          <i className='icon-briefcase' />Statistics as ListGroup
+          <i className='icon-briefcase' />Statistics (Collapsible Lists)
+          {Users.canUpdate({ collection: Statistics, user: currentUser, document }) &&
+            <div className='float-right'>
+              <LinkContainer to='/statistics/edit'>
+                <Button variant='secondary'>Edit</Button>
+              </LinkContainer>
+            </div>}
         </Card.Header>
         <Card.Body>
-          <ListGroup>
-            {results.map(statistic => <ListGroup.Item key={statistic._id}><Components.StatisticsEditForm documentId={statistic._id} currentUser={currentUser} /></ListGroup.Item>)}
-          </ListGroup>
+          <Row>
+            <Col>
+              <Button onClick={() => toggle(0)} variant='outline-success'>
+                Open/Close Episodics
+              </Button>
+              <Fade appear in={open[0]} mountOnEnter timeout={50} unmountOnExit>
+                <ListGroup className='statistics-lists'>
+                  {theStats.episodics.map(stat => (<Item key={stat.date} {...stat} />))}
+                </ListGroup>
+              </Fade>
+            </Col>
+            <Col>
+              <Button onClick={() => toggle(1)} variant='outline-primary'>
+                Open/Close Features
+              </Button>
+              <Fade appear in={open[1]} mountOnEnter timeout={50} unmountOnExit>
+                <ListGroup className='statistics-lists'>
+                  {theStats.features.map(stat => (<Item key={stat.date} {...stat} />))}
+                </ListGroup>
+              </Fade>
+            </Col>
+            <Col>
+              <Button onClick={() => toggle(2)} variant='outline-warning'>
+                Open/Close Pilots
+              </Button>
+              <Fade appear in={open[2]} mountOnEnter timeout={50} unmountOnExit>
+                <ListGroup className='statistics-lists'>
+                  {theStats.pilots.map(stat => (<Item key={stat.date} {...stat} />))}
+                </ListGroup>
+              </Fade>
+            </Col>
+            <Col>
+              <Button onClick={() => toggle(3)} variant='outline-danger'>
+                Open/Close Others
+              </Button>
+              <Fade appear in={open[3]} mountOnEnter timeout={50} unmountOnExit>
+                <ListGroup className='statistics-lists'>
+                  {theStats.others.map(stat => (<Item key={stat.date} {...stat} />))}
+                </ListGroup>
+              </Fade>
+            </Col>
+          </Row>
+          <Row className='mt-4'>
+            <Col>
+              <Button onClick={() => toggle(0)} variant='outline-success'>
+                Open/Close Episodics
+              </Button>
+            </Col>
+            <Col>
+              <Button onClick={() => toggle(1)} variant='outline-primary'>
+                Open/Close Features
+              </Button>
+            </Col>
+            <Col>
+              <Button onClick={() => toggle(2)} variant='outline-warning'>
+                Open/Close Pilots
+              </Button>
+            </Col>
+            <Col>
+              <Button onClick={() => toggle(3)} variant='outline-danger'>
+                Open/Close Others
+              </Button>
+            </Col>
+          </Row>
         </Card.Body>
-        {hasMore &&
-          <Card.Footer>
-            {loadingMore
-              ? <Components.Loading />
-              : <Button onClick={e => { e.preventDefault(); loadMore() }}>Load More ({count}/{totalCount})</Button>}
-          </Card.Footer>}
       </Card>
     </div>
   )
@@ -36,4 +119,11 @@ const options = {
   collection: Statistics
 }
 
-registerComponent('StatisticsList', StatisticsList, withCurrentUser, [withMulti, options])
+registerComponent({
+  name: 'StatisticsList',
+  component: StatisticsList,
+  hocs: [
+    withCurrentUser,
+    [withMulti, options]
+  ]
+})
