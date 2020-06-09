0b3a9b5123a707527b56706ccd9067488c4bb3a1
Kevin Ashworth <kevinashworth@yahoo.com>
2020-06-08 20:34:23 -0700

    Use it throughout rest of LatestUpdates

--- packages/v8/lib/components/common/LatestUpdates/LatestUpdates.js
+++ packages/v8/lib/components/common/LatestUpdates/LatestUpdates.js
@@ -1,5 +1,5 @@
 import { Components, registerComponent, withAccess, withMulti } from 'meteor/vulcan:core'
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import Card from 'react-bootstrap/Card'
@@ -41,45 +41,43 @@ const MyLoader = ({ cardClass }) => {
   )
 }
 
-class LatestContactUpdates extends Component {
-  render () {
-    if (this.props.networkStatus !== 8 && this.props.networkStatus !== 7) {
-      return <MyLoader cardClass='card-accent-warning' />
-    }
-
-    const contacts = this.props.results || []
-
-    return (
-      <Row className='row-cols-xs-1 row-cols-sm-2 row-cols-md-3 row-cols-xxxl-6'>
-        {contacts.map(contact => {
-          const isItNew = moment(contact.updatedAt).isBefore(moment(contact.createdAt).add(1, 'day'))
-          let displayHtml = isItNew
-            ? '<b>New!</b> Contact added '
-            : 'Contact updated '
-          displayHtml += moment(contact.updatedAt).format(DATE_FORMAT_SHORT_FRIENDLY)
-          return (
-            <Col className='my-2' key={contact._id}>
-              <Card className='card-accent-warning'>
-                <Card.Header className='text-truncate'>
-                  <b><Link to={`/contacts/${contact._id}/${contact.slug}`}>{contact.displayName}</Link></b>
-                  <small className='float-right'>{contact.theAddress.location}</small>
-                </Card.Header>
-                <Card.Body className='text-truncate'>
-                  <Card.Text>
-                    {contact.title}<br />
-                    {pluralize('Project', contact.projects ? contact.projects.length : 0, true)}
-                  </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                  <small className='text-muted'><Markup content={displayHtml} /></small>
-                </Card.Footer>
-              </Card>
-            </Col>
-          )
-        })}
-      </Row>
-    )
+const LatestContactUpdates = (props) => {
+  if (props.networkStatus !== 8 && props.networkStatus !== 7) {
+    return <MyLoader cardClass='card-accent-warning' />
   }
+
+  const contacts = props.results || []
+
+  return (
+    <Row className='row-cols-xs-1 row-cols-sm-2 row-cols-md-3 row-cols-xxxl-6'>
+      {contacts.map(contact => {
+        const isItNew = moment(contact.updatedAt).isBefore(moment(contact.createdAt).add(1, 'day'))
+        let displayHtml = isItNew
+          ? '<b>New!</b> Contact added '
+          : 'Contact updated '
+        displayHtml += moment(contact.updatedAt).format(DATE_FORMAT_SHORT_FRIENDLY)
+        return (
+          <Col className='my-2' key={contact._id}>
+            <Card className='card-accent-warning'>
+              <Card.Header className='text-truncate'>
+                <b><Link to={`/contacts/${contact._id}/${contact.slug}`}>{contact.displayName}</Link></b>
+                <small className='float-right'>{contact.theAddress.location}</small>
+              </Card.Header>
+              <Card.Body className='text-truncate'>
+                <Card.Text>
+                  {contact.title}<br />
+                  {pluralize('Project', contact.projects ? contact.projects.length : 0, true)}
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className='text-muted'><Markup content={displayHtml} /></small>
+              </Card.Footer>
+            </Card>
+          </Col>
+        )
+      })}
+    </Row>
+  )
 }
 
 LatestContactUpdates.propTypes = {
@@ -98,42 +96,40 @@ registerComponent({
   hocs: [[withMulti, contactsOptions]]
 })
 
-class LatestOfficeUpdates extends Component {
-  render () {
-    if (this.props.networkStatus !== 8 && this.props.networkStatus !== 7) {
-      return (<div><Components.Loading /></div>)
-    }
-
-    const offices = this.props.results || []
-
-    return (
-      <Row className='row-cols-xs-1 row-cols-sm-2 row-cols-md-3 row-cols-xxxl-6'>
-        {offices.map(office => {
-          const isItNew = moment(office.updatedAt).isBefore(moment(office.createdAt).add(1, 'day'))
-          let displayHtml = isItNew
-            ? '<b>New!</b> Office added '
-            : 'Office updated '
-          displayHtml += moment(office.updatedAt).format(DATE_FORMAT_SHORT_FRIENDLY)
-          return (
-            <Col className='my-2' key={office._id}>
-              <Card className='card-accent-primary'>
-                <Card.Header className='text-truncate d-block'>
-                  <b><Link to={`/offices/${office._id}/${office.slug}`}>{office.displayName}</Link></b>
-                </Card.Header>
-                <Card.Body className='text-truncate d-block'>
-                  {office.theCity} {office.theState}<br />
-                  {pluralize('Project', office.projects ? office.projects.length : 0, true)}
-                </Card.Body>
-                <Card.Footer>
-                  <small className='text-muted'><Markup content={displayHtml} /></small>
-                </Card.Footer>
-              </Card>
-            </Col>
-          )
-        })}
-      </Row>
-    )
+const LatestOfficeUpdates = (props) => {
+  if (props.networkStatus !== 8 && props.networkStatus !== 7) {
+    return <MyLoader cardClass='card-accent-primary' />
   }
+
+  const offices = props.results || []
+
+  return (
+    <Row className='row-cols-xs-1 row-cols-sm-2 row-cols-md-3 row-cols-xxxl-6'>
+      {offices.map(office => {
+        const isItNew = moment(office.updatedAt).isBefore(moment(office.createdAt).add(1, 'day'))
+        let displayHtml = isItNew
+          ? '<b>New!</b> Office added '
+          : 'Office updated '
+        displayHtml += moment(office.updatedAt).format(DATE_FORMAT_SHORT_FRIENDLY)
+        return (
+          <Col className='my-2' key={office._id}>
+            <Card className='card-accent-primary'>
+              <Card.Header className='text-truncate d-block'>
+                <b><Link to={`/offices/${office._id}/${office.slug}`}>{office.displayName}</Link></b>
+              </Card.Header>
+              <Card.Body className='text-truncate d-block'>
+                {office.theCity} {office.theState}<br />
+                {pluralize('Project', office.projects ? office.projects.length : 0, true)}
+              </Card.Body>
+              <Card.Footer>
+                <small className='text-muted'><Markup content={displayHtml} /></small>
+              </Card.Footer>
+            </Card>
+          </Col>
+        )
+      })}
+    </Row>
+  )
 }
 
 LatestOfficeUpdates.propTypes = {
@@ -223,42 +219,45 @@ registerComponent({
   hocs: [[withMulti, projectsOptions2]]
 })
 
-class LatestPastProjectUpdates extends Component {
-  render () {
-    if (this.props.networkStatus !== 8 && this.props.networkStatus !== 7) {
-      return (<div><Components.Loading /></div>)
-    }
-
-    const pastProjects = this.props.results || []
-
+const LatestPastProjectUpdates = (props) => {
+  if (props.networkStatus !== 8 && props.networkStatus !== 7) {
     return (
-      <Row className='row-cols-xs-1 row-cols-sm-2 row-cols-md-3 row-cols-xxxl-6'>
-        {pastProjects.map(project =>
-          <Col className='my-2' key={project._id}>
-            <Card className='card-accent-secondary'>
-              <Card.Header className='text-truncate d-block'>
-                <b><Link to={`/past-projects/${project._id}/${project.slug}`}>{project.projectTitle}</Link></b>
-              </Card.Header>
-              <Card.Body className='text-truncate d-block'>
-                {project.network && (project.projectType.indexOf('TV') === 0 || project.projectType.indexOf('Pilot') === 0)
-                  ? `${project.projectType} • ${project.network}`
-                  : `${project.projectType}`}<br />
-                {project.status}<br />
-                {project.castingCompany
-                  ? project.castingCompany
-                  : project.castingOffice
-                    ? project.castingOffice.displayName
-                    : null}
-              </Card.Body>
-              <Card.Footer>
-                <small className='text-muted'>Past Project as of {moment(project.updatedAt).format(DATE_FORMAT_SHORT_FRIENDLY)}</small>
-              </Card.Footer>
-            </Card>
-          </Col>
-        )}
-      </Row>
+      <>
+        <MyLoader cardClass='card-accent-secondary' />
+        <MyLoader cardClass='card-accent-secondary' />
+      </>
     )
   }
+
+  const pastProjects = props.results || []
+
+  return (
+    <Row className='row-cols-xs-1 row-cols-sm-2 row-cols-md-3 row-cols-xxxl-6'>
+      {pastProjects.map(project =>
+        <Col className='my-2' key={project._id}>
+          <Card className='card-accent-secondary'>
+            <Card.Header className='text-truncate d-block'>
+              <b><Link to={`/past-projects/${project._id}/${project.slug}`}>{project.projectTitle}</Link></b>
+            </Card.Header>
+            <Card.Body className='text-truncate d-block'>
+              {project.network && (project.projectType.indexOf('TV') === 0 || project.projectType.indexOf('Pilot') === 0)
+                ? `${project.projectType} • ${project.network}`
+                : `${project.projectType}`}<br />
+              {project.status}<br />
+              {project.castingCompany
+                ? project.castingCompany
+                : project.castingOffice
+                  ? project.castingOffice.displayName
+                  : null}
+            </Card.Body>
+            <Card.Footer>
+              <small className='text-muted'>Past Project as of {moment(project.updatedAt).format(DATE_FORMAT_SHORT_FRIENDLY)}</small>
+            </Card.Footer>
+          </Card>
+        </Col>
+      )}
+    </Row>
+  )
 }
 
 LatestPastProjectUpdates.propTypes = {
@@ -278,49 +277,47 @@ registerComponent({
   hocs: [[withMulti, pastProjectsOptions]]
 })
 
-class LatestUpdates extends Component {
-  render () {
-    return (
-      <div className='animated fadeIn'>
-        <Components.HeadTags title='V8: Latest Updates' />
+const LatestUpdates = () => {
+  return (
+    <div className='animated fadeIn'>
+      <Components.HeadTags title='V8: Latest Updates' />
 
-        <Card>
-          <Card.Body>
-            <Card.Title>Newest Projects Casting</Card.Title>
-            <Components.NewestProjectsAdded />
-          </Card.Body>
-        </Card>
+      <Card>
+        <Card.Body>
+          <Card.Title>Newest Projects Casting</Card.Title>
+          <Components.NewestProjectsAdded />
+        </Card.Body>
+      </Card>
 
-        <Card>
-          <Card.Body>
-            <Card.Title>Recently Updated Contacts</Card.Title>
-            <Components.LatestContactUpdates />
-          </Card.Body>
-        </Card>
+      <Card>
+        <Card.Body>
+          <Card.Title>Recently Updated Contacts</Card.Title>
+          <Components.LatestContactUpdates />
+        </Card.Body>
+      </Card>
 
-        <Card>
-          <Card.Body>
-            <Card.Title>Recently Updated Offices</Card.Title>
-            <Components.LatestOfficeUpdates />
-          </Card.Body>
-        </Card>
+      <Card>
+        <Card.Body>
+          <Card.Title>Recently Updated Offices</Card.Title>
+          <Components.LatestOfficeUpdates />
+        </Card.Body>
+      </Card>
 
-        <Card>
-          <Card.Body>
-            <Card.Title>Recently Updated Projects</Card.Title>
-            <Components.LatestProjectUpdates />
-          </Card.Body>
-        </Card>
+      <Card>
+        <Card.Body>
+          <Card.Title>Recently Updated Projects</Card.Title>
+          <Components.LatestProjectUpdates />
+        </Card.Body>
+      </Card>
 
-        <Card>
-          <Card.Body>
-            <Card.Title>Newest Past Projects</Card.Title>
-            <Components.LatestPastProjectUpdates />
-          </Card.Body>
-        </Card>
-      </div>
-    )
-  }
+      <Card>
+        <Card.Body>
+          <Card.Title>Newest Past Projects</Card.Title>
+          <Components.LatestPastProjectUpdates />
+        </Card.Body>
+      </Card>
+    </div>
+  )
 }
 
 const accessOptions = {
@@ -331,5 +328,7 @@ const accessOptions = {
 registerComponent({
   name: 'LatestUpdates',
   component: LatestUpdates,
-  hocs: [[withAccess, accessOptions]]
+  hocs: [
+    [withAccess, accessOptions]
+  ]
 })
