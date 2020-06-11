f2ea5964ada13419e3f2b978375f2dd1c14571f4
Kevin Ashworth <kevinashworth@yahoo.com>
2020-06-11 07:35:38 -0700

    Update 5 packages I'm using — why-did-you-render, interweave, moment, react-table, twilio Remove unused interweave-ssr IMPORTANT: Move whyDidYouRender to Vulcan's AppGenerator.jsx due to crash in FragmentsDashboard that I don't understand, but see https://github.com/welldone-software/why-did-you-render/issues/86

--- packages/v8/lib/client/main.js
+++ packages/v8/lib/client/main.js
@@ -1,14 +1,9 @@
-import React from 'react'
-
+/* Due to FragmentsDashboard crashing, moved whyDidYouRender to AppGenerator.jsx in Vulcan */
+// import React from 'react'
+//
 // if (process.env.NODE_ENV === 'development') {
-//   // const whyDidYouRender = require('@welldone-software/why-did-you-render');
-//   // See https://github.com/welldone-software/why-did-you-render/issues/5#issuecomment-467900253
-//   const whyDidYouRender = require('@welldone-software/why-did-you-render/dist/no-classes-transpile/umd/whyDidYouRender.min.js');
-//   whyDidYouRender(React, {
-//     trackAllPureComponents: true,
-//     include: [/^Contact/, /^Office/, /^PastProject/, /^Project/],
-//     exclude: [/NewForm$/, /FormGroup/]
-//   });
+// const whyDidYouRender = require('@welldone-software/why-did-you-render');
+//   whyDidYouRender(React);
 // }
 
 import '../modules/index.js'
