f83a831d2d3741cad4af4fefe261b39142e2bda5
Kevin <kevinashworth@yahoo.com>
2020-06-15 09:11:11 -0700

    WIP - not using v8-loader - commit just to end this branch, messy

--- .storybook/loaders/v8-loader.js
+++ .storybook/loaders/v8-loader.js
@@ -1,18 +1,37 @@
 /**
  * Load V8
  */
+// const { getOptions } = require('loader-utils')
 
-// const path = require('path');
+// module.exports = function loader (source) {
+//   const options = getOptions(this)
+//   const { packagesDir, environment } = options
+//   return `${packagesDir}/v8/lib/${environment}/main.js`
+// }
+
+// const { getOptions } = require('loader-utils')
+// module.exports = function loader (source) {
+//   const options = getOptions(this)
+
+//   const { packagesDir, environment = 'client' } = options
+//   // prefixing your packages name makes it easier to write a loader
+//   const prefix = `${packagesDir}/v8`
+//   const defaultPath = `/lib/${environment}/main.js`
+
+//   const result = source.replace(
+//     /meteor\/example-(.*?(?=\/|'|"))(.*?(?=\'|\"))/g, // match Meteor packages that are lfg packages, + the import path (without the quotes)
+//     (match, packageName, importPath) => {
+//       console.log('Found Starter example package', packageName)
+//       if (importPath) {
+//         return `${prefix}${packageName}${importPath}`
+//       }
+//       return `${prefix}${packageName}${defaultPath}`
+//     }
+//   )
+//   return result
+// }
 
 module.exports = function loader (source) {
-  // const result = path.resolve(__dirname, '../../packages/v8-alba/lib/client/main.js');
-  // // const result = path.resolve(__dirname, '../../packages/v8-alba/lib/components/offices/OfficeMini.jsx');
-  // console.log('v8-loader result:', result)
-  // return result;
-  //
-  const result = source.replace(
-    /v8-alba/,
-    '/Users/kevin/Developer/bitbucket_projects/V8-Alba/packages/v8-alba/lib/client/main.js'
-  )
-  return result
+  return '../packages/v8/lib/modules/components.js'
+  // return `../packages/v8/lib/client/main.js`
 }
