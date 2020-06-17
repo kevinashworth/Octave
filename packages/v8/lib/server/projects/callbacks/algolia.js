eb5da2d31a6a489c66a76ae61e41d66d79795294
Kevin Ashworth <kevinashworth@yahoo.com>
2020-06-16 18:22:19 -0700

    Avoid startup errors due to lack of Algolia secrets. In those cases, do nothing but return the document, per https://docs.vulcanjs.org/callbacks.html#Collection-Callbacks

--- packages/v8/lib/server/projects/callbacks/algolia.js
+++ packages/v8/lib/server/projects/callbacks/algolia.js
@@ -1,81 +1,94 @@
+/* eslint-disable no-undef */
 import { Promise } from 'meteor/promise'
 import algoliasearch from 'algoliasearch'
 import _ from 'lodash'
 import { ACTIVE_PROJECT_STATUSES_ARRAY } from '../../../modules/constants.js'
 
-const applicationid = Meteor.settings.public.algolia.ApplicationID
-const algoliaindex = Meteor.settings.private.algolia.AlgoliaIndex
-const addupdatekey = Meteor.settings.private.algolia.AddAndUpdateAPIKey
-
 export function ProjectEditUpdateAlgoliaBefore (data, { document, originalDocument }) {
-  var indexedObject = {
-    objectID: document._id
-  }
-  var dirty = false
-  if (document.summary !== originalDocument.summary) {
-    indexedObject.body = document.summary
-    dirty = true
-  }
-  if (document.notes !== originalDocument.notes) {
-    indexedObject.notes = document.notes
-    dirty = true
-  }
-  if (document.projectTitle !== originalDocument.projectTitle) {
-    indexedObject.name = document.projectTitle
-    dirty = true
-  }
-  if (document.network !== originalDocument.network) {
-    indexedObject.network = document.network
-    dirty = true
-  }
-  if (document.slug !== originalDocument.slug) {
-    indexedObject.url = `/projects/${document._id}/${document.slug}`
-    dirty = true
-  }
-  if (document.status !== originalDocument.status) {
-    if (_.includes(ACTIVE_PROJECT_STATUSES_ARRAY, document.status)) {
-      indexedObject.url = `/projects/${document._id}/${document.slug}`
-    } else {
-      indexedObject.url = `/past-projects/${document._id}/${document.slug}`
-    }
-    dirty = true
-  }
+  if (Meteor.settings.private) {
+    const applicationid = Meteor.settings.public.algolia.ApplicationID
+    const algoliaindex = Meteor.settings.private.algolia.AlgoliaIndex
+    const addupdatekey = Meteor.settings.private.algolia.AddAndUpdateAPIKey
 
-  if (dirty) {
-    const client = algoliasearch(applicationid, addupdatekey)
-    const index = client.initIndex(algoliaindex)
-    indexedObject['updatedAt'] = new Date()
-    Promise.await(
-      index.partialUpdateObject(
-        indexedObject,
-        { createIfNotExists: true }
+    var indexedObject = {
+      objectID: document._id
+    }
+    var dirty = false
+    if (document.summary !== originalDocument.summary) {
+      indexedObject.body = document.summary
+      dirty = true
+    }
+    if (document.notes !== originalDocument.notes) {
+      indexedObject.notes = document.notes
+      dirty = true
+    }
+    if (document.projectTitle !== originalDocument.projectTitle) {
+      indexedObject.name = document.projectTitle
+      dirty = true
+    }
+    if (document.network !== originalDocument.network) {
+      indexedObject.network = document.network
+      dirty = true
+    }
+    if (document.slug !== originalDocument.slug) {
+      indexedObject.url = `/projects/${document._id}/${document.slug}`
+      dirty = true
+    }
+    if (document.status !== originalDocument.status) {
+      if (_.includes(ACTIVE_PROJECT_STATUSES_ARRAY, document.status)) {
+        indexedObject.url = `/projects/${document._id}/${document.slug}`
+      } else {
+        indexedObject.url = `/past-projects/${document._id}/${document.slug}`
+      }
+      dirty = true
+    }
+
+    if (dirty) {
+      const client = algoliasearch(applicationid, addupdatekey)
+      const index = client.initIndex(algoliaindex)
+      indexedObject.updatedAt = new Date()
+      Promise.await(
+        index.partialUpdateObject(
+          indexedObject,
+          { createIfNotExists: true }
+        )
+          .then(response => {
+            console.log('partialUpdateObject response:', response)
+          })
+          .catch(error => {
+            console.error('partialUpdateObject error:', error)
+          })
       )
-      .then(response => {
-          console.log('partialUpdateObject response:', response)
-      })
-      .catch(error => {
-        console.error('partialUpdateObject error:', error)
-      })
-    )
+    }
+  } else {
+    return data
   }
 }
 
 export function ProjectCreateSaveToAlgolia (document) {
-  const indexedObject = {
-    objectID: document._id,
-    name: document.projectTitle,
-    body: document.summary,
-    notes: document.notes,
-    network: document.network,
-    url: `/projects/${document._id}/${document.slug}`,
-    updatedAt: document.createdAt,
-    boosted: 2
-  }
+  if (Meteor.settings.private) {
+    const applicationid = Meteor.settings.public.algolia.ApplicationID
+    const algoliaindex = Meteor.settings.private.algolia.AlgoliaIndex
+    const addupdatekey = Meteor.settings.private.algolia.AddAndUpdateAPIKey
 
-  const client = algoliasearch(applicationid, addupdatekey)
-  const index = client.initIndex(algoliaindex)
-  Promise.await(index.saveObject(indexedObject)
-    .then(response => console.log('saveObject response:', response))
-    .catch(error => console.error('saveObject error:', error))
-  )
+    const indexedObject = {
+      objectID: document._id,
+      name: document.projectTitle,
+      body: document.summary,
+      notes: document.notes,
+      network: document.network,
+      url: `/projects/${document._id}/${document.slug}`,
+      updatedAt: document.createdAt,
+      boosted: 2
+    }
+
+    const client = algoliasearch(applicationid, addupdatekey)
+    const index = client.initIndex(algoliaindex)
+    Promise.await(index.saveObject(indexedObject)
+      .then(response => console.log('saveObject response:', response))
+      .catch(error => console.error('saveObject error:', error))
+    )
+  } else {
+    return document
+  }
 }
