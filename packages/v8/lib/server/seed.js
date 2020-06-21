e39b26485816bf08c4eb81966b5d4180db935ff0
Kevin Ashworth <kevinashworth@yahoo.com>
2020-06-21 13:53:00 -0700

    Import seed data conditionally, dynamically

--- packages/v8/lib/server/seed.js
+++ packages/v8/lib/server/seed.js
@@ -9,11 +9,6 @@ import Patches from '../modules/patches/collection.js'
 import Projects from '../modules/projects/collection.js'
 import PastProjects from '../modules/past-projects/collection.js'
 import Statistics from '../modules/statistics/collection.js'
-import { contacts } from './seeds/generated/contacts.js'
-import { offices } from './seeds/generated/offices.js'
-import { pastprojects } from './seeds/generated/pastprojects.js'
-import { projects } from './seeds/generated/projects.js'
-import { statistics } from './seeds/_statistics.js'
 
 const createUser = async (username, email) => {
   const user = {
@@ -37,17 +32,6 @@ const createDummyUsers = async () => {
   ])
 }
 
-const seedPatch = {
-  _id: 'v4vAkMJ2EyqMmFKGw',
-  patches: [
-    {
-      date: new Date('2020-01-03T21:08:58.333Z').toUTCString(),
-      patch: { op: 'replace', path: '/addresses/0/street2', value: 'Stage 4, 5th Floor' }
-    }
-  ],
-  collectionName: 'Offices'
-}
-
 // eslint-disable-next-line no-undef
 Vulcan.removeGettingStartedContent = () => {
   Users.remove({ 'profile.isDummy': true })
@@ -64,61 +48,73 @@ Meteor.startup(() => {
   if (Contacts.find().fetch().length === 0) {
     // eslint-disable-next-line no-console
     console.log('// creating dummy contacts')
-    Promise.awaitAll(contacts.map(document => newMutation({
-      action: 'contacts.new',
-      collection: Contacts,
-      document,
-      currentUser,
-      validate: false
-    })))
-  }
-  if (Projects.find().fetch().length === 0) {
-    // eslint-disable-next-line no-console
-    console.log('// creating dummy projects')
-    Promise.awaitAll(projects.map(document => newMutation({
-      action: 'projects.new',
-      collection: Projects,
-      document,
-      currentUser,
-      validate: false
-    })))
-  }
-  if (PastProjects.find().fetch().length === 0) {
-    // eslint-disable-next-line no-console
-    console.log('// creating dummy past-projects')
-    Promise.awaitAll(pastprojects.map(document => newMutation({
-      action: 'past-projects.create',
-      collection: PastProjects,
-      document,
-      currentUser,
-      validate: false
-    })))
+    import('./generated/contacts.js').then(({ contacts }) => {
+      Promise.awaitAll(contacts.map(document => newMutation({
+        action: 'contacts.new',
+        collection: Contacts,
+        document,
+        currentUser,
+        validate: false
+      })))
+    })
   }
   if (Offices.find().fetch().length === 0) {
     // eslint-disable-next-line no-console
     console.log('// creating dummy offices')
-    Promise.awaitAll(offices.map(document => newMutation({
-      action: 'offices.new',
-      collection: Offices,
-      document,
-      currentUser,
-      validate: false
-    })))
+    import('./generated/offices.js').then(({ offices }) => {
+      Promise.awaitAll(offices.map(document => newMutation({
+        action: 'offices.new',
+        collection: Offices,
+        document,
+        currentUser,
+        validate: false
+      })))
+    })
+  }
+  if (Projects.find().fetch().length === 0) {
+    // eslint-disable-next-line no-console
+    console.log('// creating dummy projects')
+    import('./generated/projects.js').then(({ projects }) => {
+      Promise.awaitAll(projects.map(document => newMutation({
+        action: 'projects.new',
+        collection: Projects,
+        document,
+        currentUser,
+        validate: false
+      })))
+    })
+  }
+  if (PastProjects.find().fetch().length === 0) {
+    // eslint-disable-next-line no-console
+    console.log('// creating dummy past-projects')
+    import('./generated/pastprojects.js').then(({ pastprojects }) => {
+      Promise.awaitAll(pastprojects.map(document => newMutation({
+        action: 'past-projects.create',
+        collection: PastProjects,
+        document,
+        currentUser,
+        validate: false
+      })))
+    })
   }
   if (Statistics.find().fetch().length === 0) {
     // eslint-disable-next-line no-console
     console.log('// creating dummy statistics')
-    Promise.await(Statistics.insert(statistics))
+    import('./seeds/_statistics.js').then(({ statistics }) => {
+      Promise.await(Statistics.insert(statistics))
+    })
   }
   if (Patches.find().fetch().length === 0) {
     // eslint-disable-next-line no-console
     console.log('// creating dummy patches')
-    Promise.await(newMutation({
-      action: 'patches.new',
-      collection: Patches,
-      document: seedPatch,
-      currentUser,
-      validate: false
-    }))
+    import('./seeds/_patch.js').then(({ seedPatch }) => {
+      Promise.await(newMutation({
+        action: 'patches.new',
+        collection: Patches,
+        document: seedPatch,
+        currentUser,
+        validate: false
+      }))
+    })
   }
 })
