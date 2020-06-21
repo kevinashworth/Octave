699d5df0686a0b72dd9cb4e30c71b3b20f9c8a39
Kevin Ashworth <kevinashworth@yahoo.com>
2020-06-20 18:32:55 -0700

    Fixes after testing

--- mockaroo/mockaroo.sh
+++ mockaroo/mockaroo.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+mkdir generated
 node get-from-schema --schema=contacts
-node 1-upload-contacts-csv.js
+node 1-upload-contacts-csv
 node get-from-schema -s offices
-node 2-upload-offices-csv.js
+node 2-upload-offices-csv
 node get-from-schema --schema=pastprojects
 node get-from-schema --schema=projects
 
-cp -R generated ../packages/v8/lib/server/seeds
+mv generated ../packages/v8/lib/server/seeds
