9bdefc7537c625b592e62c16d699b5d833716b9a
Kevin Ashworth <kevinashworth@yahoo.com>
2020-07-07 20:37:47 -0700

    Make a schema for extendCollection
    Instead of lots of addField

--- packages/v8/lib/modules/users/collection.js
+++ packages/v8/lib/modules/users/collection.js
@@ -1,9 +1,14 @@
+import { extendCollection } from 'meteor/vulcan:core'
 import './schema.js'
 import './fragments.js' // import after schema for getRequiredFields to find all `mustComplete`
-
+import schema2 from './schema2.js'
 import Users from 'meteor/vulcan:users'
 
 Users.createGroup('participants')
 Users.createGroup('pending')
+
+extendCollection(Users, {
+  schema: schema2
+})
 
 export default Users
