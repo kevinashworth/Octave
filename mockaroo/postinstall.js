4c31edd850d604e0e7824c9268ebe7dab325246c
Kevin Ashworth <kevinashworth@yahoo.com>
2020-12-10 08:08:06 -0800

    Add mockaroo settings and postinstall

---
+++ mockaroo/postinstall.js
@@ -0,0 +1,30 @@
+var fs = require('fs')
+var path = require('path')
+
+var settingsFilePath = path.resolve(__dirname, '../settings.json')
+
+var settings = {}
+
+try {
+  var settingsFile = fs.readFileSync(settingsFilePath)
+  settings = JSON.parse(settingsFile)
+} catch (err) {
+  console.log(`Error reading ${settingsFilePath}: ${err}`)
+}
+
+if (settings.mockaroo) {
+  if (settings.mockaroo.generateAndDownload) {
+    settings.mockaroo = {
+      generateAndDownload: false,
+      seedDatabase: false,
+      initializeAlgolia: false,
+      updateAlgolia: true
+    }
+  }
+  try {
+    var data = JSON.stringify(settings, null, 2)
+    fs.writeFileSync(settingsFilePath, data)
+  } catch (err) {
+    console.log(`Error writing ${settingsFilePath}: ${err}`)
+  }
+}
