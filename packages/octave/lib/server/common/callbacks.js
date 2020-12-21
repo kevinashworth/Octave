09212a2757f1113b64c0555d5ab65b1ffddb7bc4
Kevin Ashworth <kevinashworth@yahoo.com>
2020-12-21 15:45:49 -0800

    New async delete callbacks: deleteAlgoliaObject, deleteComments
    - These are reusable across C, O, P, PP, so they are in a new 'common' folder

---
+++ packages/octave/lib/server/common/callbacks.js
@@ -0,0 +1,55 @@
+import { Connectors, getSetting } from 'meteor/vulcan:core'
+import algoliasearch from 'algoliasearch'
+import includes from 'lodash/includes'
+import log from 'loglevel'
+import { logger } from '../logger'
+import Comments from '../../modules/comments/collection'
+import { DBS } from '../../modules/constants'
+import { getMongoProvider } from '../../modules/helpers'
+
+// callbacks.delete.async
+export function deleteAlgoliaObject ({ document }) {
+  const db = getMongoProvider()
+  let shouldUpdateAlgolia = true
+  let warning = 'Updating Algolia'
+  if (getSetting('mockaroo.initializeAlgolia')) {
+    shouldUpdateAlgolia = false
+    warning = 'Skipping object update to initialize Algolia as a batch.'
+  } if (getSetting('mockaroo.updateAlgolia')) {
+    shouldUpdateAlgolia = true
+    warning = 'Updating object on Algolia.'
+  } else if (!includes(DBS, db)) {
+    shouldUpdateAlgolia = false
+    warning = 'No Atlas, no mLab. Not sending to Algolia'
+  }
+
+  if (!shouldUpdateAlgolia) {
+    log.debug(warning)
+    return
+  }
+
+  if (Meteor.settings.private && Meteor.settings.private.algolia) {
+    const applicationid = Meteor.settings.public.algolia.ApplicationID
+    const algoliaindex = Meteor.settings.public.algolia.AlgoliaIndex
+    const addupdatekey = Meteor.settings.private.algolia.AddAndUpdateAPIKey
+
+    const objectID = document._id
+    logger.info('About to deleteAlgoliaObject', objectID)
+
+    const client = algoliasearch(applicationid, addupdatekey)
+    const index = client.initIndex(algoliaindex)
+    index
+      .deleteObject(objectID)
+      .then(response => { console.log('projects deleteObject response:', response) })
+      .catch(error => { console.error('projects deleteObject error:', JSON.stringify(error, null, 2)) })
+  }
+}
+
+// callbacks.delete.async
+export const deleteComments = ({ document }) => {
+  logger.info('Deleting comments for document', document._id)
+  Comments.find({ objectId: { $eq: document._id } }).forEach(comment => {
+    logger.info('Deleting comment', comment._id)
+    Connectors.delete(Comments, comment._id)
+  })
+}
