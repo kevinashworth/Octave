3d2f45687f99969c6b69d8a2fa2ee8be84600369
Kevin Ashworth <kevinashworth@yahoo.com>
2020-12-24 11:43:28 -0800

    Improve Write Stats button UI. Add permissions. Fix date bug, error bug.

--- packages/octave/lib/components/statistics/StatisticsCurrent.jsx
+++ packages/octave/lib/components/statistics/StatisticsCurrent.jsx
@@ -1,4 +1,5 @@
-import { Components, Utils, useMulti2, useSingle2, useUpdate2, withMessages } from 'meteor/vulcan:core'
+import { Components, Utils, useCurrentUser, useMulti2, useSingle2, useUpdate2, withMessages } from 'meteor/vulcan:core'
+import Users from 'meteor/vulcan:users'
 import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
@@ -6,7 +7,7 @@ import ListGroup from 'react-bootstrap/ListGroup'
 import moment from 'moment'
 import Projects from '../../modules/projects/collection.js'
 import Statistics from '../../modules/statistics/collection.js'
-import { PROJECT_TYPES_EPISODICS, PROJECT_TYPES_FEATURES, PROJECT_TYPES_PILOTS, PROJECT_TYPES_OTHERS } from '../../modules/constants.js'
+import { DATE_FORMAT_LONG, PROJECT_TYPES_EPISODICS, PROJECT_TYPES_FEATURES, PROJECT_TYPES_PILOTS, PROJECT_TYPES_OTHERS } from '../../modules/constants.js'
 
 function latestStatIsFromToday (theStatsArray) {
   const latestDate = theStatsArray[theStatsArray.length - 1].date
@@ -14,9 +15,11 @@ function latestStatIsFromToday (theStatsArray) {
   return moment(latestDate).isSame(today, 'day')
 }
 
-const StatisticsCurrent = (props) => {
-  const { flash } = props
-  const [disabled, setDisabled] = useState(false)
+const StatisticsCurrent = ({ flash }) => {
+  const { currentUser } = useCurrentUser()
+  const defaultEnabled = Users.canUpdate({ collection: Statistics, user: currentUser })
+  const [disabled, setDisabled] = useState(!defaultEnabled)
+
   const { loading: loadingE, error: errorE, totalCount: episodicsCasting } = useMulti2({
     collection: Projects,
     input: {
@@ -27,7 +30,6 @@ const StatisticsCurrent = (props) => {
       }
     }
   })
-  const [updateStatistic] = useUpdate2({ collection: Statistics });
 
   const { loading: loadingF, error: errorF, totalCount: featuresCasting } = useMulti2({
     collection: Projects,
@@ -69,6 +71,8 @@ const StatisticsCurrent = (props) => {
     }
   })
 
+  const [updateStatistic] = useUpdate2({ collection: Statistics })
+
   if (loadingE || loadingF || loadingP || loadingO || loadingS) {
     return <Components.Loading />
   }
@@ -77,7 +81,7 @@ const StatisticsCurrent = (props) => {
   if (errorF) console.log('errorF:', errorF)
   if (errorP) console.log('errorP:', errorP)
   if (errorO) console.log('errorO:', errorO)
-  if (errorO) console.log('errorS:', errorS)
+  if (errorS) console.log('errorS:', errorS)
 
   const writeStatistics = () => {
     const newStats = {
@@ -87,30 +91,36 @@ const StatisticsCurrent = (props) => {
       others: theStats.others
     }
     Utils.removeProperty(newStats, '__typename')
+
     if (latestStatIsFromToday(newStats.episodics)) {
       newStats.episodics.pop()
     }
     newStats.episodics.push({ date: new Date(), quantity: episodicsCasting })
+
     if (latestStatIsFromToday(newStats.features)) {
       newStats.features.pop()
     }
     newStats.features.push({ date: new Date(), quantity: featuresCasting })
+
     if (latestStatIsFromToday(newStats.pilots)) {
       newStats.pilots.pop()
     }
     newStats.pilots.push({ date: new Date(), quantity: pilotsCasting })
+
     if (latestStatIsFromToday(newStats.others)) {
       newStats.others.pop()
     }
     newStats.others.push({ date: new Date(), quantity: othersCasting })
+
     updateStatistic({
       input: {
         id: 'HSEC7MWC9RFCJLEMP',
         data: newStats
       }
     }).then((results) => {
+      const updatedAt = moment(results.data.updateStatistic.data.updatedAt).format(DATE_FORMAT_LONG)
       flash({
-        message: `Stats updated as of ${results.data.updateStatistic.data.updatedAt}`,
+        message: `Statistics updated as of ${updatedAt}`,
         type: 'success'
       })
       setDisabled(true)
@@ -128,11 +138,19 @@ const StatisticsCurrent = (props) => {
       <Card.Body>
         <ListGroup className='statistics-lists'>
           <ListGroup.Item>Episodics Casting: {episodicsCasting}</ListGroup.Item>
-          <ListGroup.Item>Features Casting: {featuresCasting}</ListGroup.Item>
+          <ListGroup.Item>Features Casting: <span data-cy='features-casting'>{featuresCasting}</span></ListGroup.Item>
           <ListGroup.Item>Pilots Casting: {pilotsCasting}</ListGroup.Item>
           <ListGroup.Item>Others Casting: {othersCasting}</ListGroup.Item>
         </ListGroup>
-        <Button onClick={() => writeStatistics()} disabled={disabled} data-cy='write-statistics'>Write These Statistics to Database</Button>
+        <Button
+          className='mt-2'
+          data-cy='write-statistics'
+          disabled={disabled}
+          onClick={() => writeStatistics()}
+          variant={disabled ? 'light' : 'primary'}
+        >
+          Write These Statistics to Database
+        </Button>
       </Card.Body>
     </Card>
   )
