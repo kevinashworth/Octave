3fbfd4bbf371d6dc8245b7906f99cc36e4d79912
Kevin Ashworth <kevinashworth@yahoo.com>
2020-12-25 08:24:56 -0800

    Add handleUpdateContacts to handleAddContacts and handleRemoveContacts

--- packages/octave/lib/server/projects/callbacks/contacts.js
+++ packages/octave/lib/server/projects/callbacks/contacts.js
@@ -2,7 +2,9 @@ import { Connectors } from 'meteor/vulcan:core'
 import Contacts from '../../../modules/contacts/collection.js'
 import differenceWith from 'lodash/differenceWith'
 import findIndex from 'lodash/findIndex'
+import isEqual from 'lodash/isEqual'
 import remove from 'lodash/remove'
+import log from 'loglevel'
 import { isEmptyValue } from '../../../modules/helpers.js'
 
 // callbacks.create.async
@@ -17,13 +19,21 @@ export const createProjectUpdateContacts = ({ document }) => {
 export const updateProjectUpdateContacts = ({ document, originalDocument }) => {
   const newProject = document
   const oldProject = originalDocument
-  const contactsThatWereAdded = differenceWith(newProject.contacts, oldProject.contacts, isSameContact)
-  const contactsThatWereRemoved = differenceWith(oldProject.contacts, newProject.contacts, isSameContact)
+  const contactsThatWereAdded = differenceWith(newProject.contacts, oldProject.contacts, isSameContactById)
+  const contactsThatWereRemoved = differenceWith(oldProject.contacts, newProject.contacts, isSameContactById)
+  const contactsThatWereUpdated = differenceWith(newProject.contacts, oldProject.contacts, isUpdatedContact)
+  log.debug('updateProjectUpdateContacts:')
+  log.debug('contactsThatWereRemoved:', contactsThatWereRemoved)
+  log.debug('contactsThatWereAdded:', contactsThatWereAdded)
+  log.debug('contactsThatWereUpdated:', contactsThatWereUpdated)
   if (!isEmptyValue(contactsThatWereRemoved)) {
     handleRemoveContacts(contactsThatWereRemoved, newProject._id)
   }
   if (!isEmptyValue(contactsThatWereAdded)) {
     handleAddContacts(contactsThatWereAdded, newProject)
+  }
+  if (!isEmptyValue(contactsThatWereUpdated)) {
+    handleUpdateContacts(contactsThatWereUpdated, newProject)
   }
 }
 
@@ -89,6 +99,51 @@ const handleRemoveContacts = (contacts, projectId) => {
   })
 }
 
-const isSameContact = (a, b) => {
+const handleUpdateContacts = (contacts, project) => {
+  const projectId = project._id
+  contacts.forEach(updatedContact => {
+    const contact = Contacts.findOne(updatedContact.contactId)
+    console.log('contact title possibles.')
+    console.log('project.contacts:', project.contacts)
+    console.log('updatedContact?.contactTitle:', updatedContact?.contactTitle)
+    const updatedProject = {
+      projectId,
+      projectTitle: project.projectTitle,
+      titleForProject: updatedContact?.contactTitle
+    }
+    let updatedProjects = []
+    // case 1: nothing there
+    if (isEmptyValue(contact.projects)) {
+      updatedProjects = [updatedProject]
+    } else {
+      updatedProjects = contact.projects
+      const i = findIndex(contact.projects, { projectId })
+      if (i < 0) {
+        // case 2: add to it
+        updatedProjects.push(updatedProject)
+      } else {
+        // case 3: it is there and needs to be updated <-- different from Add function!
+        updatedProjects[i] = updatedProject
+      }
+    }
+    Connectors.update(Contacts, contact._id, {
+      $set: {
+        projects: updatedProjects,
+        updatedAt: new Date()
+      }
+    })
+  })
+}
+
+const isSameContactById = (a, b) => {
   return a.contactId === b.contactId
+}
+
+const isUpdatedContact = (a, b) => {
+  if (isSameContactById) {
+    const result = isEqual(a, b)
+    return result
+  } else {
+    return false
+  }
 }
