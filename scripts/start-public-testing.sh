542030df10261e63e80d7e9b3f5b1e7fd9c70170
Kevin Ashworth <kevinashworth@yahoo.com>
2021-01-30 11:21:35 -0800

    Update public-testing

--- scripts/start-public-testing.sh
+++ scripts/start-public-testing.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-MONGO_URL='mongodb+srv://read-only-testing-account:valid\during\february\2021@cluster0.dk7n0.mongodb.net/Triad?retryWrites=true&w=majority' meteor --port 4004 --settings settings-triad.json
+MONGO_URL='mongodb+srv://read-only-testing-account:valid-during-february-2021@cluster0.dk7n0.mongodb.net/Triad?retryWrites=true&w=majority' meteor --port 4004 --settings settings-public-testing.json
